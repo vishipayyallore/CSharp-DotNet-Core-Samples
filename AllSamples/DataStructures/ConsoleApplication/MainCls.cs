0568ffab89acbd397f107a357fdd491d5b664bd9
Viswanatha Swamy <vishipayyallore@yahoo.com>
2016-09-13 20:32:44 +0530

    Adding first console application

---
+++ AllSamples/DataStructures/ConsoleApplication/MainCls.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace DataStructureConsoleApp
+{
+
+    public class MainCls
+    {
+        public static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Cyan;
+
+            Console.WriteLine("\n\nPress any key ...");
+            Console.ReadKey();
+        }
+    }
+
+}
