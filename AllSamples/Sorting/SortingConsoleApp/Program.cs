1273c738f51db5f7eca2befb629e715c80c748e1
Viswanatha Swamy <vishipayyallore@yahoo.com>
2016-09-15 22:29:53 +0530

    Added Console and Library for Sorting Programs

---
+++ AllSamples/Sorting/SortingConsoleApp/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using static System.Console;
+
+namespace SortingConsoleApp
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            ForegroundColor = ConsoleColor.Cyan;
+
+            WriteLine("\n\nPress any key ...");
+            ReadKey();
+        }
+    }
+}
