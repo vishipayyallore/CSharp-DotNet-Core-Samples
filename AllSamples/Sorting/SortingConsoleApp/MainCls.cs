7e1b0a79fadef8f95010e6c7ea21e90ce0694e48
Viswanatha Swamy <vishipayyallore@yahoo.com>
2016-09-15 22:54:34 +0530

    ,

--- AllSamples/Sorting/SortingConsoleApp/MainCls.cs
+++ AllSamples/Sorting/SortingConsoleApp/MainCls.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Dynamic;
+using System.Linq;
 using static System.Console;
 
 namespace SortingConsoleApp
@@ -9,8 +11,30 @@ namespace SortingConsoleApp
         {
             ForegroundColor = ConsoleColor.Cyan;
 
+            var arrayElements = ReadLine().Trim().Split(' ').Select(int.Parse).ToArray();
+
             WriteLine("\n\nPress any key ...");
             ReadKey();
         }
+
+
+        private static void MergeSubArrays(int[] numbers, int left, int mid, int right)
+        {
+            var tempArray = new int[numbers.Length;
+
+        }
+
+        private static void MergeSort(int[] numbers, int left, int right)
+        {
+            if (left > right)
+            {
+                var mid = (left + right) / 2;
+                MergeSort(numbers, left, mid);
+                MergeSort(numbers, mid, right);
+
+                MergeSubArrays(numbers, left, mid, right);
+            }
+        }
+
     }
 }
--- AllSamples/Sorting/Sortings/MergeSort.cs
+++ AllSamples/Sorting/Sortings/MergeSort.cs
@@ -1,6 +1,12 @@
 ﻿namespace Sortings
 {
 
+    /*
+        1. If the list is of length 0 or 1, then it is already sorted. Otherwise: 
+        2. Divide the unsorted list into two sublists of about half the size. 
+        3. Sort each sublist recursively by re-applying merge sort. 
+        4. Merge the two sublists back into one sorted list. 
+     */
     public class MergeSort
     {
         public static void Run()
