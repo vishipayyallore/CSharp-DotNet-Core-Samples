9b518f738e8def852bc0ffc0137144b268aad18c
Viswanatha Swamy <vishipayyallore@yahoo.com>
2016-09-18 23:01:53 +0530

    Bubble Sort program

--- AllSamples/Sorting/SortingConsoleApp/MainCls.cs
+++ AllSamples/Sorting/SortingConsoleApp/MainCls.cs
@@ -9,6 +9,9 @@ namespace SortingConsoleApp
         public static void Main(string[] args)
         {
             ForegroundColor = ConsoleColor.Cyan;
+            //89 76 45 92 67 12 99 | 7 4 5 2 | 4 83 9 6 34 45 | 3 8 7 5 2 1 9 6 4
+
+            BubbleSort.Run();
 
             //var arrayElements = ReadLine().Trim().Split(' ').Select(int.Parse).ToArray();
             var numbers = new[] { 3, 8, 7, 5, 2, 1, 9, 6, 4 };
