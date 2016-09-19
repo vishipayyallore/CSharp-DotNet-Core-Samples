46525d79dfda4e6afdd43e753d4e2fcd910782d5
Viswanatha Swamy <vishipayyallore@yahoo.com>
2016-09-19 14:45:17 +0530

    Selection Sort Added.

--- AllSamples/Sorting/SortingConsoleApp/MainCls.cs
+++ AllSamples/Sorting/SortingConsoleApp/MainCls.cs
@@ -10,12 +10,15 @@ namespace SortingConsoleApp
         {
             ForegroundColor = ConsoleColor.Cyan;
             //89 76 45 92 67 12 99 | 7 4 5 2 | 4 83 9 6 34 45 | 3 8 7 5 2 1 9 6 4
+            
+            for (var i = 0; i < 4; i++)
+            {
+                BubbleSort.Run();
 
-            //Bubble Sort
-            BubbleSort.Run();
+                MergeSort.Run();
 
-            //Merge Sort
-            MergeSort.Run();
+                SelectionSort.Run();
+            }
 
             WriteLine("\n\nPress any key ...");
             ReadKey();
---
+++ AllSamples/Sorting/Sortings/SelectionSort.cs
@@ -0,0 +1,29 @@
+﻿using System.Linq;
+using static System.Console;
+
+namespace Sortings
+{
+    public class SelectionSort
+    {
+        public static void Run()
+        {
+            var arrayData = ReadLine().Trim().Split(' ').Select(int.Parse).ToArray();
+            for (var iCtr = 0; iCtr < arrayData.Length - 1; iCtr++)
+            {
+                var itemIndex = iCtr;
+                for (var jCtr = iCtr; jCtr < arrayData.Length; jCtr++)
+                {
+                    if (arrayData[jCtr] < arrayData[itemIndex])
+                    {
+                        itemIndex = jCtr;
+                    }
+                }
+                var temp = arrayData[iCtr];
+                arrayData[iCtr] = arrayData[itemIndex];
+                arrayData[itemIndex] = temp;
+                WriteLine($"Pass {iCtr + 1}: {string.Join(" ", arrayData)}");
+            }
+            WriteLine($"Selection Sorted: {string.Join(" ", arrayData)}\n");
+        }
+    }
+}
