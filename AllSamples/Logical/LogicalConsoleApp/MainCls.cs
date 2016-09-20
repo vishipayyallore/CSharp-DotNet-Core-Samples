9312e1bed5e25e6f8320248e86e59fe8355cd12a
Viswanatha Swamy <vishipayyallore@yahoo.com>
2016-09-20 22:18:24 +0530

    .

---
+++ AllSamples/Logical/LogicalConsoleApp/MainCls.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using static System.Console;
+
+namespace LogicalConsoleApp
+{
+    public class MainCls
+    {
+        public static void Main(string[] args)
+        {
+            ForegroundColor = ConsoleColor.Cyan;
+
+            WriteLine("\n\nPress any key ...");
+            ReadKey();
+        }
+    }
+}
