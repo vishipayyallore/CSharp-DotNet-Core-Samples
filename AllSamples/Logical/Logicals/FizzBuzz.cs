150d604262c08932851ae16262a2fd91dad47392
Viswanatha Swamy <vishipayyallore@yahoo.com>
2016-09-20 22:16:01 +0530

    FizzBuzz Program

---
+++ AllSamples/Logical/Logicals/FizzBuzz.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Linq;
+using static System.Console;
+
+namespace Logicals
+{
+    public class FizzBuzz
+    {
+        public static void Run()
+        {
+            var numberOfElements = int.Parse(ReadLine().Trim());
+            if ((numberOfElements < 1) || (numberOfElements > 10))
+            {
+                return;
+            }
+            var arrayValues = ReadLine().Trim().Split(' ');
+            var arrayEndValues = arrayValues.Select(int.Parse).ToArray();
+
+
+            for (var jCtr = 0; jCtr < numberOfElements; jCtr++)
+            {
+                var startValue = 1;
+                var endValue = arrayEndValues[jCtr];
+
+                endValue -= startValue - 1;
+                for (var iCtr = 0; iCtr < endValue; iCtr++)
+                {
+                    var toPrint = startValue % 3 == 0 ? "Fizz" : "";
+                    toPrint += startValue % 5 == 0 ? "Buzz" : "";
+                    toPrint = (startValue % 3 != 0) && (startValue % 5 != 0) ? startValue.ToString() : toPrint;
+                    WriteLine(toPrint);
+                    startValue++;
+                }
+            }
+        }
+    }
+}
