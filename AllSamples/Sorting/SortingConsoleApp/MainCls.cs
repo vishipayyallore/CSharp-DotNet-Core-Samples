5fa68c5ea4af57a376c2266534e2ef17180db937
Viswanatha Swamy <vishipayyallore@yahoo.com>
2016-09-25 17:34:11 +0530

    .

--- AllSamples/Sorting/SortingConsoleApp/MainCls.cs
+++ AllSamples/Sorting/SortingConsoleApp/MainCls.cs
@@ -1,15 +1,15 @@
 ﻿using System;
-using static System.Console;
 using Sortings;
+using static System.Console;
 
 namespace SortingConsoleApp
 {
     public class MainCls
     {
+        //89 76 45 92 67 12 99 | 7 4 5 2 | 4 83 9 6 34 45 | 3 8 7 5 2 1 9 6 4
         public static void Main(string[] args)
         {
             ForegroundColor = ConsoleColor.Cyan;
-            //89 76 45 92 67 12 99 | 7 4 5 2 | 4 83 9 6 34 45 | 3 8 7 5 2 1 9 6 4
             
             for (var i = 0; i < 4; i++)
             {
@@ -25,10 +25,6 @@ namespace SortingConsoleApp
             WriteLine("\n\nPress any key ...");
             ReadKey();
         }
-
-        
-
-        
 
     }
 }
