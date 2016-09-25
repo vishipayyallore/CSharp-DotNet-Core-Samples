0faade7fa846bb9508abf27dfbc933e15088547a
Viswanatha Swamy <vishipayyallore@yahoo.com>
2016-09-25 17:47:27 +0530

    .

--- AllSamples/DataStructures/ConsoleApplication/MainCls.cs
+++ AllSamples/DataStructures/ConsoleApplication/MainCls.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Linq;
 using SingleDArray;
-using TwoDArray;
 using static System.Console;
 
 namespace DataStructureConsoleApp
@@ -20,16 +19,16 @@ namespace DataStructureConsoleApp
             //MonksLoveForFood
             MonksLoveForFood.Run1();
 
-            //Two D Array
-            Transpose.Run();
+            ////Two D Array
+            //Transpose.Run();
 
-            //Print SD Array in Reverse
-            var numberOfRepetations = int.Parse(ReadLine().Trim());
-            for ( ; numberOfRepetations>0; numberOfRepetations--)
-            {
-                var arrayElements = ReadLine().Trim().Split(' ').Select(int.Parse).ToArray();
-                PrintReverse.Run(arrayElements);
-            }
+            ////Print SD Array in Reverse
+            //var numberOfRepetations = int.Parse(ReadLine().Trim());
+            //for ( ; numberOfRepetations>0; numberOfRepetations--)
+            //{
+            //    var arrayElements = ReadLine().Trim().Split(' ').Select(int.Parse).ToArray();
+            //    PrintReverse.Run(arrayElements);
+            //}
 
             WriteLine("\n\nPress any key ...");
             ReadKey();
