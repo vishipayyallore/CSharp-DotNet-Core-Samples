31b52b428fad3adccddfd050dd7f7faa9dd73652
Viswanatha Swamy <vishipayyallore@yahoo.com>
2016-10-15 17:46:11 +0530

    .

--- AllSamples/DataStructures/ConsoleApplication/MainCls.cs
+++ AllSamples/DataStructures/ConsoleApplication/MainCls.cs
@@ -13,40 +13,7 @@ namespace DataStructureConsoleApp
         {
             ForegroundColor = ConsoleColor.Cyan;
 
-            var firstLineInput = ReadLine().Trim().Split(' ').Select(int.Parse).ToArray();
-            var arrayLength = firstLineInput[0];
-            var numberOfQueries = firstLineInput[1];
-
-            var arrayValues = ReadLine().Trim().Split(' ').Select(int.Parse).ToArray();
-            for (var iCtr = 0; iCtr < numberOfQueries; iCtr++)
-            {
-                var currentQuery = ReadLine().Trim().Split(' ').Select(int.Parse).ToArray();
-                var tempArray = arrayValues;
-                var sum = 0.0;
-                var jCtr = 0;
-                var queryType = currentQuery[0];
-                switch (queryType)
-                {
-                    case 0:
-                        jCtr = currentQuery[1] - 1;
-                        arrayLength = currentQuery[2] - currentQuery[1] + 1;
-                        break;
-                    case 1:
-                        var indexToChange = currentQuery[1] - 1;
-                        tempArray[indexToChange] = 1 - arrayValues[indexToChange];
-                        break;
-                }
-                
-                for (; jCtr < arrayLength; jCtr++)
-                {
-                    sum += (tempArray[jCtr] * (Math.Pow(2, jCtr)));
-                }
-                if (currentQuery[0] == 0)
-                {
-                    WriteLine("{0}", ((sum % 2 == 1) ? "ODD" : "EVEN"));
-                }
-
-            }
+            
 
             //MonksLoveForFood
             WriteLine("MonksLoveForFood.cs");
