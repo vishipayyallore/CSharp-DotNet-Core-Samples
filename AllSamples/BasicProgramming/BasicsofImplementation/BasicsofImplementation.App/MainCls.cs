2122ff4927961243db42e1b506481c71037ae7f3
Viswanatha Swamy <vishipayyallore@yahoo.com>
2016-10-22 00:53:10 +0530

    .

--- AllSamples/BasicProgramming/BasicsofImplementation/BasicsofImplementation.App/MainCls.cs
+++ AllSamples/BasicProgramming/BasicsofImplementation/BasicsofImplementation.App/MainCls.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Linq;
+using BasicsofImplementation.Lib;
 using static System.Console;
 
 namespace BasicsofImplementation.App
@@ -8,6 +10,22 @@ namespace BasicsofImplementation.App
         public static void Main(string[] args)
         {
             ForegroundColor = ConsoleColor.Cyan;
+
+            var numberOfElements = int.Parse(ReadLine().Trim());
+            var arrayElements = ReadLine().Trim().Split(' ').Select(int.Parse).ToArray();
+            double value1 = 0, value2 = 0, value3 = 0;
+
+            for (var iCtr = 0; iCtr < numberOfElements; iCtr+=3)
+            {
+                value1 += arrayElements[iCtr];
+                if (iCtr + 1 >= numberOfElements) continue;
+                value2 += arrayElements[iCtr+1];
+                if(iCtr + 2 >= numberOfElements) continue;
+                value3 += arrayElements[iCtr+2];
+            }
+            WriteLine($"{value1} {value2} {value3}");
+
+            CountDigits.Run();
 
             WriteLine("\n\nPress any key ...");
             ReadKey();
--- AllSamples/BasicProgramming/BasicsofImplementation/BasicsofImplementation.Lib/CountDigits.cs
+++ AllSamples/BasicProgramming/BasicsofImplementation/BasicsofImplementation.Lib/CountDigits.cs
@@ -1,9 +1,29 @@
-﻿namespace BasicsofImplementation.Lib
+﻿using System.Collections.Generic;
+using static System.Console;
+
+namespace BasicsofImplementation.Lib
 {
     public class CountDigits
     {
-        public CountDigits()
+        #region Methods.
+        public static void Run()
         {
+            var digitCounts = new Dictionary<char, int>
+            {
+                {'0', 0 },{'1', 0 },{'2', 0 },{'3', 0 },{'4', 0 },{'5', 0 },{'6', 0 },{'7', 0 },{'8', 0 },{'9', 0 }
+            };
+
+            var data = ReadLine().Trim();
+            foreach (var digit in data)
+            {
+                digitCounts[digit] += 1;
+            }
+
+            foreach (var keyValuePair in digitCounts)
+            {
+                WriteLine($"{keyValuePair.Key} {keyValuePair.Value}");
+            }
         }
+        #endregion
     }
 }
