267403a7d9e71e122390491b6f8fce86a1d77d8c
Viswanatha Swamy <vishipayyallore@yahoo.com>
2016-10-22 15:46:21 +0530

    Print hackerearth

--- AllSamples/BasicProgramming/BasicsofImplementation/BasicsofImplementation.App/MainCls.cs
+++ AllSamples/BasicProgramming/BasicsofImplementation/BasicsofImplementation.App/MainCls.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using BasicsofImplementation.Lib;
 using static System.Console;
@@ -10,6 +11,8 @@ namespace BasicsofImplementation.App
         public static void Main(string[] args)
         {
             ForegroundColor = ConsoleColor.Cyan;
+
+            Printhackerearth.Run();
 
             LittleJhoolPsychicPowers.Run();
 
---
+++ AllSamples/BasicProgramming/BasicsofImplementation/BasicsofImplementation.Lib/Printhackerearth.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using static System.Console;
+
+namespace BasicsofImplementation.Lib
+{
+    public class Printhackerearth
+    {
+        #region Methods.
+        public static void Run()
+        {
+            var baseCounts = new Dictionary<char, int>
+            {
+                {'h', 2 },{'a', 2 },{'c', 1 },{'k', 1 },{'e', 2 },{'r', 2 },{'t', 1 }
+            };
+            var dataCounts = new Dictionary<char, int>
+            {
+                {'h', 0 },{'a', 0 },{'c', 0 },{'k', 0 },{'e', 0 },{'r', 0 },{'t', 0 }
+            };
+
+            var numberOfCharacters = int.Parse(ReadLine().Trim());
+            var data = ReadLine().Trim();
+            foreach (var digit in data)
+            {
+                if (!dataCounts.ContainsKey(digit))
+                {
+                    dataCounts.Add(digit, 1);
+                }
+                else
+                {
+                    dataCounts[digit] += 1;
+                }
+            }
+
+            var count = 1;
+            var exitLoop = false;
+            while (!exitLoop)
+            {
+                if (baseCounts.Any(keyValuePair => dataCounts[keyValuePair.Key] < (keyValuePair.Value * count)))
+                {
+                    exitLoop = true;
+                }
+                if (!exitLoop) { count++; }
+            }
+
+            WriteLine($"{--count}");
+        }
+        #endregion
+    }
+}
