ffe29f4f01e60f7afd470e6617993c12dc022e5e
Viswanatha Swamy <vishipayyallore@yahoo.com>
2016-10-30 15:34:23 +0530

    .

--- AllSamples/Challenges/CodeMonk_Arrays_N_Strings/ArraysNStrings.App/MainCls.cs
+++ AllSamples/Challenges/CodeMonk_Arrays_N_Strings/ArraysNStrings.App/MainCls.cs
@@ -11,21 +11,7 @@ namespace ArraysNStrings.App
         {
             ForegroundColor = Cyan;
 
-            var numberOfItems = int.Parse(ReadLine().Trim());
-            for ( ; numberOfItems > 0; numberOfItems--)
-            {
-                
-                var data = ReadLine().Trim();
-                var output = $"YES {((data.Length % 2 == 0) ? "EVEN" : "ODD")}";
-                var jCtr = 0;
-                for (var iCtr = data.Length - 1; iCtr >= 0; iCtr--, jCtr++)
-                {
-                    if (data[iCtr].Equals(data[jCtr])) continue;
-                    output = "NO";
-                    break;
-                }
-                WriteLine($"{output}");
-            }
+            Palindrome.Run();
 
             Welcome.Run();
 
---
+++ AllSamples/Challenges/CodeMonk_Arrays_N_Strings/ArraysNStrings.Lib/Palindrome.cs
@@ -0,0 +1,29 @@
+﻿using static System.Console;
+
+namespace ArraysNStrings.Lib
+{
+
+    public class Palindrome
+    {
+        #region Methods
+        public static void Run()
+        {
+            var numberOfItems = int.Parse(ReadLine().Trim());
+            for (; numberOfItems > 0; numberOfItems--)
+            {
+                var data = ReadLine().Trim();
+                var output = $"YES {((data.Length % 2 == 0) ? "EVEN" : "ODD")}";
+                var jCtr = 0;
+                for (var iCtr = data.Length - 1; iCtr >= 0; iCtr--, jCtr++)
+                {
+                    if (data[iCtr].Equals(data[jCtr])) continue;
+                    output = "NO";
+                    break;
+                }
+                WriteLine($"{output}");
+            }
+        }
+        #endregion
+    }
+
+}
