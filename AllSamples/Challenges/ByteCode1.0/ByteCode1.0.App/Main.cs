fcdf65f1f2d8d2fe6dffcfe217c377638cc77423
Viswanatha Swamy <vishipayyallore@yahoo.com>
2016-11-03 13:43:06 +0530

    .

---
+++ AllSamples/Challenges/ByteCode1.0/ByteCode1.0.App/Main.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ByteCode1._0.App
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ AllSamples/Challenges/ByteCode1.0/ByteCode1.0.Lib/Class1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ByteCode1._0.Lib
+{
+    public class Class1
+    {
+        public Class1()
+        {
+        }
+    }
+}
