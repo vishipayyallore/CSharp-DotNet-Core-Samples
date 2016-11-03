3533f632a166b3ff43dad1d6547eaf2104659583
Viswanatha Swamy <vishipayyallore@yahoo.com>
2016-11-03 23:04:08 +0530

    .

--- AllSamples/Challenges/ByteCode1.0/ByteCode1.0.App/Main.cs
+++ AllSamples/Challenges/ByteCode1.0/ByteCode1.0.App/Main.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Text;
 using static System.Console;
 using static System.ConsoleColor;
 
@@ -20,11 +21,48 @@ namespace ByteCode1._0.App
                 {'S', 0 },{'T', 0 },{'U', 0 },{'V', 0 },{'W', 0 },{'X', 0 },{'Y', 0 },{'Z', 0 },
             };
 
-            byte[] inputBuffer = new byte[14096];
+            var inputData = @"";
+
+            byte[] inputBuffer = new byte[4096];
             Stream inputStream = Console.OpenStandardInput();
             Console.SetIn(new StreamReader(inputStream, Console.InputEncoding, false, inputBuffer.Length));
 
-            var data = ReadLine().Trim().ToUpperInvariant();
+            StringBuilder sb = new StringBuilder();
+            StringReader sr = new StringReader(sb.ToString());
+            Console.WriteLine("Reading Profile");
+
+            //Peek to see if the next character exists
+            while (sr.Peek() > -1)
+            {
+                //Read a line from the string and display it on the
+                //console
+                Console.WriteLine(sr.ReadLine());
+            }
+
+            Console.WriteLine("Data Read Complete!");
+            //Close the string
+            sr.Dispose();
+
+            //var data = ReadLine().Trim().ToUpperInvariant();
+            var data = string.Empty;
+
+            do
+            {
+                //input code
+
+                //Check for exit conditions
+                data = ReadLine().Trim().ToUpperInvariant();
+            } while (!string.IsNullOrWhiteSpace(data));
+
+            while (!string.IsNullOrWhiteSpace(data = Console.ReadLine()))
+            {
+                //but here it seems to be an infinite loop 
+                foreach (var currentChar in data.Where(currentChar => dataCounts.ContainsKey(currentChar)))
+                {
+                    dataCounts[currentChar] += 1;
+                }
+            }
+
             foreach (var currentChar in data.Where(currentChar => dataCounts.ContainsKey(currentChar)))
             {
                 dataCounts[currentChar] += 1;
