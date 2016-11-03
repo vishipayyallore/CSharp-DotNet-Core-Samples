07ad612d1636aa9fd4243dbdacd56ce243ef4dc2
Viswanatha Swamy <vishipayyallore@yahoo.com>
2016-11-03 15:24:08 +0530

    .

--- AllSamples/Challenges/ByteCode1.0/ByteCode1.0.App/Main.cs
+++ AllSamples/Challenges/ByteCode1.0/ByteCode1.0.App/Main.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
-using System.Threading.Tasks;
+using static System.Console;
+using static System.ConsoleColor;
 
 namespace ByteCode1._0.App
 {
@@ -9,6 +11,32 @@ namespace ByteCode1._0.App
     {
         public static void Main(string[] args)
         {
+            ForegroundColor = Cyan;
+
+            var dataCounts = new Dictionary<char, int>
+            {
+                {'A', 0 },{'B', 0 },{'C', 0 },{'D', 0 },{'E', 0 },{'F', 0 },{'G', 0 },{'H', 0 },{'I', 0 },
+                {'J', 0 },{'K', 0 },{'L', 0 },{'M', 0 },{'N', 0 },{'O', 0 },{'P', 0 },{'Q', 0 },{'R', 0 },
+                {'S', 0 },{'T', 0 },{'U', 0 },{'V', 0 },{'W', 0 },{'X', 0 },{'Y', 0 },{'Z', 0 },
+            };
+
+            byte[] inputBuffer = new byte[14096];
+            Stream inputStream = Console.OpenStandardInput();
+            Console.SetIn(new StreamReader(inputStream, Console.InputEncoding, false, inputBuffer.Length));
+
+            var data = ReadLine().Trim().ToUpperInvariant();
+            foreach (var currentChar in data.Where(currentChar => dataCounts.ContainsKey(currentChar)))
+            {
+                dataCounts[currentChar] += 1;
+            }
+
+            foreach (var keyValuePair in dataCounts.Where(k => k.Value > 0))
+            {
+                WriteLine($"{keyValuePair.Key} : {keyValuePair.Value}");
+            }
+
+            WriteLine("\n\nPress any key ...");
+            ReadKey();
         }
     }
 }
--- AllSamples/Challenges/ByteCode1.0/ByteCode1.0.Lib/Class1.cs
+++ AllSamples/Challenges/ByteCode1.0/ByteCode1.0.Lib/Class1.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace ByteCode1._0.Lib
+﻿namespace ByteCode1._0.Lib
 {
     public class Class1
     {
