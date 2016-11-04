a0cfc2a78f53f0c7be1ef5ad00c065f4b34685f7
Viswanatha Swamy <vishipayyallore@yahoo.com>
2016-11-04 21:40:44 +0530

    .

--- AllSamples/Challenges/ByteCode1.0/ByteCode1.0.App/Main.cs
+++ AllSamples/Challenges/ByteCode1.0/ByteCode1.0.App/Main.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using static System.Console;
 using static System.ConsoleColor;
 
@@ -10,6 +11,55 @@ namespace ByteCode1._0.App
 {
     public class Program
     {
+        /// <summary>
+        /// The time limit for the puzzle
+        /// </summary>
+        const int TIME_LIMIT_SECONDS = 5;
+
+        /// <summary>
+        /// Set this static field to true to quit the game
+        /// </summary>
+        static bool quit = false;
+
+        /// <summary>
+        /// The main gameloop
+        /// </summary>
+        static void MainLoop()
+        {
+            int elapsedMilliseconds = 0;
+            int totalMilliseconds = TIME_LIMIT_SECONDS * 1000;
+            const int INTERVAL = 100;
+
+            while (elapsedMilliseconds < totalMilliseconds && !quit)
+            {
+                // Sleep for a short period
+                //Thread.Sleep(INTERVAL);
+                elapsedMilliseconds += INTERVAL;
+
+                HandleInput();
+
+                //PrintRemainingTime(elapsedMilliseconds, totalMilliseconds);
+            }
+
+            Console.SetCursorPosition(0, 20);
+            Console.WriteLine(Environment.NewLine + Environment.NewLine
+                + "Game over! You found {0} words.");
+        }
+
+        /// <summary>
+        /// Handle any waiting user keystrokes 
+        /// </summary>
+        static void HandleInput()
+        {
+            //Thread.Sleep(50);
+            if (Console.KeyAvailable)
+            {
+                ConsoleKeyInfo keyInfo = Console.ReadKey(true);
+                string key = keyInfo.KeyChar.ToString().ToUpper();
+                WriteLine(key);
+            }
+        }
+
         public static void Main(string[] args)
         {
             ForegroundColor = Cyan;
@@ -20,6 +70,54 @@ namespace ByteCode1._0.App
                 {'J', 0 },{'K', 0 },{'L', 0 },{'M', 0 },{'N', 0 },{'O', 0 },{'P', 0 },{'Q', 0 },{'R', 0 },
                 {'S', 0 },{'T', 0 },{'U', 0 },{'V', 0 },{'W', 0 },{'X', 0 },{'Y', 0 },{'Z', 0 },
             };
+
+            
+
+            while (true)
+            {
+                if (Console.KeyAvailable)
+                {
+                    ConsoleKeyInfo key = Console.ReadKey(true);
+                    switch (key.Key)
+                    {
+                        case ConsoleKey.F1:
+                            Console.WriteLine("You pressed F1!");
+                            break;
+                        default:
+                            break;
+                    }
+                }
+                else
+                {
+                    int elapsedMilliseconds = 0;
+                    int totalMilliseconds = TIME_LIMIT_SECONDS * 1000;
+                    const int INTERVAL = 100;
+                    WriteLine("Not entered");
+                    while (elapsedMilliseconds < totalMilliseconds && !quit)
+                    {
+                        // Sleep for a short period
+                        Thread.Sleep(INTERVAL);
+                        elapsedMilliseconds += INTERVAL;
+                        Write($"{elapsedMilliseconds} ");
+                        //HandleInput();
+
+                        //PrintRemainingTime(elapsedMilliseconds, totalMilliseconds);
+                    }
+                    break;
+                }
+                // Do something more useful
+            }
+
+            MainLoop();
+
+            Console.WriteLine("Press ESC to stop");
+            do
+            {
+                while (!Console.KeyAvailable)
+                {
+                    // Do something
+                }
+            } while (Console.ReadKey(true).Key != ConsoleKey.Escape);
 
             var inputData = @"";
 
