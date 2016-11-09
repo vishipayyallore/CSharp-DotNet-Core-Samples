e69dd5e0666b649b0f71509e8eea7f2e626c72f8
Viswanatha Swamy <vishipayyallore@yahoo.com>
2016-11-09 22:48:03 +0530

    .

--- AllSamples/Challenges/ByteCode1.0/ByteCode1.0.App/Main.cs
+++ AllSamples/Challenges/ByteCode1.0/ByteCode1.0.App/Main.cs
@@ -38,8 +38,6 @@ namespace ByteCode1._0.App
         private static void HandleInput()
         {
             if (!Console.KeyAvailable) return;
-            //var keyInfo = Console.ReadKey(true);
-            //InputContent.Append(keyInfo.KeyChar.ToString().ToUpper());
             var keyInfo = ReadLine();
             InputContent.Append(keyInfo);
         }
@@ -52,14 +50,6 @@ namespace ByteCode1._0.App
             Console.SetIn(new StreamReader(inputStream, Console.InputEncoding, false, inputBuffer.Length));
 
             MainLoop();
-            //var output = InputContent.ToString().Replace('\n', ' ');
-            //foreach (var currentChar in output.ToCharArray())
-            //{
-            //    if (_dataCounts.ContainsKey(currentChar))
-            //    {
-            //        _dataCounts[currentChar] += 1;
-            //    }
-            //}
 
             foreach (var currentChar in InputContent.ToString().Trim().ToUpperInvariant().Where(currentChar => _dataCounts.ContainsKey(currentChar)))
             {
@@ -72,65 +62,6 @@ namespace ByteCode1._0.App
             }
 
             Console.WriteLine("Press ESC to stop");
-            //do
-            //{
-            //    while (!Console.KeyAvailable)
-            //    {
-            //        // Do something
-            //    }
-            //} while (Console.ReadKey(true).Key != ConsoleKey.Escape);
-
-            //var inputData = @"";
-
-            //byte[] inputBuffer = new byte[4096];
-            //Stream inputStream = Console.OpenStandardInput();
-            //Console.SetIn(new StreamReader(inputStream, Console.InputEncoding, false, inputBuffer.Length));
-
-            //StringBuilder sb = new StringBuilder();
-            //StringReader sr = new StringReader(sb.ToString());
-            //Console.WriteLine("Reading Profile");
-
-            ////Peek to see if the next character exists
-            //while (sr.Peek() > -1)
-            //{
-            //    //Read a line from the string and display it on the
-            //    //console
-            //    Console.WriteLine(sr.ReadLine());
-            //}
-
-            //Console.WriteLine("Data Read Complete!");
-            ////Close the string
-            //sr.Dispose();
-
-            ////var data = ReadLine().Trim().ToUpperInvariant();
-            //var data = string.Empty;
-
-            //do
-            //{
-            //    //input code
-
-            //    //Check for exit conditions
-            //    data = ReadLine().Trim().ToUpperInvariant();
-            //} while (!string.IsNullOrWhiteSpace(data));
-
-            //while (!string.IsNullOrWhiteSpace(data = Console.ReadLine()))
-            //{
-            //    //but here it seems to be an infinite loop 
-            //    foreach (var currentChar in data.Where(currentChar => dataCounts.ContainsKey(currentChar)))
-            //    {
-            //        dataCounts[currentChar] += 1;
-            //    }
-            //}
-
-            //foreach (var currentChar in data.Where(currentChar => dataCounts.ContainsKey(currentChar)))
-            //{
-            //    dataCounts[currentChar] += 1;
-            //}
-
-            //foreach (var keyValuePair in dataCounts.Where(k => k.Value > 0))
-            //{
-            //    WriteLine($"{keyValuePair.Key} : {keyValuePair.Value}");
-            //}
 
             WriteLine("\n\nPress any key ...");
             ReadKey();
