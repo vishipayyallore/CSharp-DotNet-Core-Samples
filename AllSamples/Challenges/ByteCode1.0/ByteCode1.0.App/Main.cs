5e0ddd79ad290f4aedb7b0152d90b179b0bd6ae5
Viswanatha Swamy <vishipayyallore@yahoo.com>
2016-11-09 23:26:43 +0530

    .

--- AllSamples/Challenges/ByteCode1.0/ByteCode1.0.App/Main.cs
+++ AllSamples/Challenges/ByteCode1.0/ByteCode1.0.App/Main.cs
@@ -13,13 +13,38 @@ namespace ByteCode1._0.App
     public class Program
     {
 
-        
-
-        
-
         public static void Main(string[] args)
         {
             ForegroundColor = Cyan;
+            
+            var fruitsDictionary = new SortedDictionary<int, int>();
+            var fruitsPrice = ReadLine().Trim().Split(' ').Select(int.Parse).ToArray();
+            var fruitsQuantity = ReadLine().Trim().Split(' ').Select(int.Parse).ToArray();
+            var amountAvailable = int.Parse(ReadLine().Trim());
+            var totalFruits = 0;
+
+            for (var iCtr = 0; iCtr < fruitsPrice.Length; iCtr++)
+            {
+                fruitsDictionary.Add(fruitsPrice[iCtr], fruitsQuantity[iCtr]);
+            }
+
+            fruitsDictionary.Keys.CopyTo(fruitsPrice,0);
+            fruitsDictionary.Values.CopyTo(fruitsQuantity, 0);
+
+            ProcessPurchase:
+            for (var iCtr = 0; iCtr < fruitsPrice.Length; iCtr++)
+            {
+                if ((amountAvailable < fruitsPrice[iCtr]) || (fruitsQuantity[iCtr] <= 0)) continue;
+                totalFruits++;
+                fruitsQuantity[iCtr]--;
+                amountAvailable -= fruitsPrice[iCtr];
+            }
+
+            if (amountAvailable >= fruitsPrice[0] && fruitsQuantity[0] > 0)
+            {
+                goto ProcessPurchase;
+            }
+            WriteLine($"{totalFruits}");
             
 
             Console.WriteLine("Press ESC to stop");
