825c4745fbd70cb1a82ef5adb97e960413bba404
Viswanatha Swamy <vishipayyallore@yahoo.com>
2016-11-25 23:00:29 +0530

    .

--- AllSamples/Challenges/AllChallenges/AllChallenges.App/MainCls.cs
+++ AllSamples/Challenges/AllChallenges/AllChallenges.App/MainCls.cs
@@ -15,30 +15,7 @@ namespace AllChallenges.App
         {
             ForegroundColor = ConsoleColor.Cyan;
 
-            var dataCounts = new Dictionary<char, int>
-            {
-                {'a', 1 },{'b', 2 },{'c', 3 },{'d', 4 },{'e', 5 },{'f', 6 },{'g', 7 },{'h', 8 },{'i', 9 },
-                {'j', 10 },{'k', 11 },{'l', 12 },{'m', 13 },{'n', 14 },{'o', 15 },{'p', 16 },{'q', 17 },{'r', 18 },
-                {'s', 19 },{'t', 20 },{'u', 21 },{'v', 22 },{'w', 23 },{'x', 24 },{'y', 25 },{'z', 26 }
-            };
-
-            var numberOfRepeats = int.Parse(ReadLine().Trim());
-
-            while (numberOfRepeats > 0)
-            {
-                var jCtr = 0;
-                var data = ReadLine().Trim();
-                for (var iCtr = data.Length - 1; iCtr >= 0; iCtr--, jCtr++)
-                {
-                    var index = (dataCounts[data[iCtr]] + dataCounts[data[jCtr]]) % 26;
-                    index = (index == 0) ? 26 : index;
-                    //Write($"{dataCounts[data[iCtr]]} {dataCounts[data[jCtr]]} {dataCounts[data[iCtr]] + dataCounts[data[jCtr]]} {dataCounts.FirstOrDefault(v => v.Value == index).Key}"); 
-                    Write($"{dataCounts.FirstOrDefault(v => v.Value == index).Key}");
-                }
-                WriteLine();
-                numberOfRepeats--;
-            }
-            
+            AdditionAintSimple.Run();
 
             MinMax.Run();
 
