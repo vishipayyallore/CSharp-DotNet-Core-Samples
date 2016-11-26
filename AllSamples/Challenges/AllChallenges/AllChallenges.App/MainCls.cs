f1db454723c83cb646e2d18b52470d43970e3c8d
Viswanatha Swamy <vishipayyallore@yahoo.com>
2016-11-26 22:52:42 +0530

    .

--- AllSamples/Challenges/AllChallenges/AllChallenges.App/MainCls.cs
+++ AllSamples/Challenges/AllChallenges/AllChallenges.App/MainCls.cs
@@ -15,32 +15,7 @@ namespace AllChallenges.App
         {
             ForegroundColor = ConsoleColor.Cyan;
 
-            var numberOfRepeats = int.Parse(ReadLine().Trim());
-            while (numberOfRepeats > 0)
-            {
-                var numberOfSoldiers = int.Parse(ReadLine().Trim());
-                var bobsArmy = ReadLine().Trim().Split(' ').Select(int.Parse).ToArray();
-                var alicesArmy = ReadLine().Trim().Split(' ').Select(int.Parse).ToArray();
-                var bobsArmyCount = 0;
-                var alicesArmyCount = 0;
-                for (var iCtr = 0; iCtr < numberOfSoldiers; iCtr++)
-                {
-                    if (bobsArmy[iCtr] >= bobsArmyCount)
-                    {
-                        bobsArmyCount = bobsArmy[iCtr];
-                    }
-                    if (alicesArmy[iCtr] >= alicesArmyCount)
-                    {
-                        alicesArmyCount = alicesArmy[iCtr];
-                    }
-                }
-
-                var output = (bobsArmyCount == alicesArmyCount)
-                    ? "Tie"
-                    : (bobsArmyCount > alicesArmyCount) ? "Bob" : "Alice";
-                WriteLine($"{output}");
-                numberOfRepeats--;
-            }
+            War.Run();
 
             AdditionAintSimple.Run();
 
---
+++ AllSamples/Challenges/AllChallenges/AllChallenges.Lib/War.cs
@@ -0,0 +1,38 @@
+﻿using System.Linq;
+using static System.Console;
+
+namespace AllChallenges.Lib
+{
+    public class War
+    {
+        public static void Run()
+        {
+            var numberOfRepeats = int.Parse(ReadLine().Trim());
+            while (numberOfRepeats > 0)
+            {
+                var numberOfSoldiers = int.Parse(ReadLine().Trim());
+                var bobsArmy = ReadLine().Trim().Split(' ').Select(int.Parse).ToArray();
+                var alicesArmy = ReadLine().Trim().Split(' ').Select(int.Parse).ToArray();
+                var bobsArmyCount = 0;
+                var alicesArmyCount = 0;
+                for (var iCtr = 0; iCtr < numberOfSoldiers; iCtr++)
+                {
+                    if (bobsArmy[iCtr] >= bobsArmyCount)
+                    {
+                        bobsArmyCount = bobsArmy[iCtr];
+                    }
+                    if (alicesArmy[iCtr] >= alicesArmyCount)
+                    {
+                        alicesArmyCount = alicesArmy[iCtr];
+                    }
+                }
+
+                var output = (bobsArmyCount == alicesArmyCount)
+                    ? "Tie"
+                    : (bobsArmyCount > alicesArmyCount) ? "Bob" : "Alice";
+                WriteLine($"{output}");
+                numberOfRepeats--;
+            }
+        }
+    }
+}
