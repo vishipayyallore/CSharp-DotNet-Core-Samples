5d348c6d69e3e6b46eb6ce58e529a6521ff89239
Viswanatha Swamy <vishipayyallore@yahoo.com>
2016-11-29 22:58:25 +0530

    .

---
+++ AllSamples/Challenges/AllChallenges/AllChallenges.Lib/CompilerVersion.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using static System.Console;
+
+namespace AllChallenges.Lib
+{
+    public class CompilerVersion
+    {
+
+        public static void Run()
+        {
+            string inputLine;
+            while ((inputLine = ReadLine().Trim()) != null)
+            {
+                var commentIndex = inputLine.IndexOf(@"//", StringComparison.Ordinal);
+                inputLine = commentIndex != -1 ? string.Format("{0}{1}", inputLine.Substring(0, commentIndex).Replace("->", "."), inputLine.Substring(commentIndex)) : inputLine.Replace("->", ".");
+                WriteLine($"{inputLine}");
+            }
+        }
+    }
+}
