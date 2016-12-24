871da1f2e087e0ae0a5e475edbd7ede9e0fe35eb
Viswanatha Swamy <vishipayyallore@yahoo.com>
2016-12-24 22:09:41 +0530

    .

---
+++ AllSamples/Challenges/AllChallenges/AllChallenges.Lib/EvenOddIndexedChars.cs
@@ -0,0 +1,26 @@
+﻿using static System.Console;
+
+namespace AllChallenges.Lib
+{
+    public class EvenOddIndexedChars
+    {
+        public static void Run()
+        {
+            var numberOfItems = int.Parse(ReadLine().Trim());
+            while (numberOfItems-- > 0)
+            {
+                var input = ReadLine().Trim();
+                for (var iCtr = 0; iCtr < input.Length; iCtr += 2)
+                {
+                    Write($"{input[iCtr]}");
+                }
+                Write($" ");
+                for (var iCtr = 1; iCtr < input.Length; iCtr += 2)
+                {
+                    Write($"{input[iCtr]}");
+                }
+                WriteLine();
+            }
+        }
+    }
+}
