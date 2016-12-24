9124b3f308eddb37ae7e33575f418f177fcf1d0b
Viswanatha Swamy <vishipayyallore@yahoo.com>
2016-12-24 22:07:50 +0530

    .

--- AllSamples/Challenges/AllChallenges/AllChallenges.App/MainCls.cs
+++ AllSamples/Challenges/AllChallenges/AllChallenges.App/MainCls.cs
@@ -14,6 +14,22 @@ namespace AllChallenges.App
         {
             ForegroundColor = ConsoleColor.Cyan;
 
+            var numberOfItems = int.Parse(ReadLine().Trim());
+            while (numberOfItems-- > 0)
+            {
+                var input = ReadLine().Trim();
+                for (var iCtr = 0; iCtr < input.Length; iCtr += 2)
+                {
+                    Write($"{input[iCtr]}");
+                }
+                Write($" ");
+                for (var iCtr = 1; iCtr < input.Length; iCtr += 2)
+                {
+                    Write($"{input[iCtr]}");
+                }
+                WriteLine();
+            }
+
             PersonDemo.Run();
 
             MinSumMaxSum.Run();
