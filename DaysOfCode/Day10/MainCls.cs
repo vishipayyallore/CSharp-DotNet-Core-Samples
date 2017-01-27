ac9fe90f4ca3169af128a91256c3dc9d18a0f16b
Viswanatha Swamy <vishipayyallore@yahoo.com>
2017-01-27 10:08:11 +0530

    .

---
+++ DaysOfCode/Day10/MainCls.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using static System.Console;
+
+namespace Day10
+{
+    public class MainCls
+    {
+        public static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Cyan;
+
+            WriteLine("\n\nPress any key.");
+            ReadKey();
+        }
+    }
+}
