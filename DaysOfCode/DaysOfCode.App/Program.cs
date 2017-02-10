698300c8a266d5e8119360d321525b3c6a8322cc
Viswanatha Swamy <vishipayyallore@yahoo.com>
2017-02-10 10:43:55 +0530

    .

---
+++ DaysOfCode/DaysOfCode.App/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace DaysOfCode.App
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Cyan;
+
+            WriteLine("\n\nPress any key.");
+            ReadKey();
+        }
+    }
+}
---
+++ DaysOfCode/DaysOfCode.App/Properties/AssemblyInfo.cs
@@ -0,0 +1,19 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("DaysOfCode.App")]
+[assembly: AssemblyTrademark("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("f3032ea8-6c4b-457d-81b0-0a2832e9ece0")]
