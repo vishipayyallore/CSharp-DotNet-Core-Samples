d946af2027a04e48b817c0a5e75c636f75528dba
Viswanatha Swamy <vishipayyallore@yahoo.com>
2017-02-10 17:07:30 +0530

    .

---
+++ DaysOfCode/DaysOfCode.App/BinaryNumbers.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+
+namespace DaysOfCode.App
+{
+
+    public class BinaryNumbers
+    {
+
+        #region Methods
+        public static string BinaryToDecimal(int number)
+        {
+            var stackData = new Stack<int>();
+
+            while (number > 0)
+            {
+                stackData.Push(number % 2);
+                number /= 2;
+            }
+
+            return string.Join("", stackData.ToArray());
+        }
+        #endregion
+
+    }
+}
--- DaysOfCode/DaysOfCode.App/MainCls.cs
+++ DaysOfCode/DaysOfCode.App/MainCls.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using static System.Console;
 
 namespace DaysOfCode.App
@@ -8,6 +9,21 @@ namespace DaysOfCode.App
         public static void Main(string[] args)
         {
             Console.ForegroundColor = ConsoleColor.Cyan;
+            var stackData = new Stack<int>();
+            var number = int.Parse(ReadLine().Trim());
+
+            while(number > 0)
+            {
+                stackData.Push(number % 2);
+                number /= 2;
+            }
+
+            var dat1a = string.Join("", stackData.ToArray());
+
+            foreach(var data in stackData)
+            {
+                Write($"{data}");
+            }
 
             WriteLine("\n\nPress any key.");
             ReadKey();
