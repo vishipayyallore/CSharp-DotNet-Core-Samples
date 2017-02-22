6e8bb3c713c5c5c942db7172ebbae967a68b358a
Viswanatha Swamy <vishipayyallore@yahoo.com>
2017-02-22 22:24:55 +0530

    .

---
+++ DaysOfCode/Day2Library/DataTypes.cs
@@ -0,0 +1,26 @@
+﻿using static System.Console;
+
+namespace Day2Library
+{
+
+    public class DataTypes
+    {
+
+        const int number1 = 4;
+        const double value1 = 4.0;
+        const string firstPart = "Hello World, ";
+
+        public static void Run()
+        {
+            int number2 = int.Parse(ReadLine().Trim());
+            double value2 = double.Parse(ReadLine().Trim());
+            string secondPart = ReadLine().Trim();
+
+            WriteLine($"{number1 + number2}");
+            WriteLine($"{value1 + value2}");
+            WriteLine($"{firstPart + secondPart}");
+        }
+
+    }
+
+}
---
+++ DaysOfCode/Day2Library/Properties/AssemblyInfo.cs
@@ -0,0 +1,18 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Day2Library")]
+[assembly: AssemblyTrademark("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("1a9c41e3-0831-4ada-9aa2-8800b834cf98")]
