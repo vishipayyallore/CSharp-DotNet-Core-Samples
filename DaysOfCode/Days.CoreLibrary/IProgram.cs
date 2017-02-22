a25724dd1a851eb2486e40845bb5fe197f39c603
Viswanatha Swamy <vishipayyallore@yahoo.com>
2017-02-22 22:30:25 +0530

    .

---
+++ DaysOfCode/Days.CoreLibrary/IProgram.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Days.CoreLibrary
+{
+    public interface IProgram
+    {
+        #region Methods
+        void Run();
+        #endregion
+    }
+}
