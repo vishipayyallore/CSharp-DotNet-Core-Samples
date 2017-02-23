326072c0ac5482ab4f9db78763bc51196bf14ab0
Viswanatha Swamy <vishipayyallore@yahoo.com>
2017-02-23 21:08:48 +0530

    .

---
+++ DaysOfCode/Days.Programs/HelloWorld.cs
@@ -0,0 +1,15 @@
+﻿using Days.CoreLibrary;
+using static System.Console;
+
+namespace Day1Library
+{
+    public class HelloWorld : IProgram
+    {
+        public void Run()
+        {
+            var input = ReadLine();
+
+            WriteLine($"Hello, World.\n{input}");
+        }
+    }
+}
