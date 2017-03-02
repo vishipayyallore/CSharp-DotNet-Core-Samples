ca666e0d1eead08c58ac770a0fc1eea90917b272
Viswanatha Swamy <vishipayyallore@yahoo.com>
2017-03-02 20:55:10 +0530

    .

--- DaysOfCode/DaysOfCode.App/MainCls.cs
+++ DaysOfCode/DaysOfCode.App/MainCls.cs
@@ -11,30 +11,42 @@ namespace DaysOfCode.App
         {
             Console.ForegroundColor = ConsoleColor.Cyan;
 
+            var output = string.Empty;
+            var number = int.Parse(ReadLine().Trim());
+            if(number % 2 == 1)
+            {
+                output = "Weird";
+            }
+            else
+            {
+                output = (number >= 6 && number <= 20) ? "Weird" : "Not Weird";
+            }
+            WriteLine($"{output}");
+
             new Operators().Run();
             new HelloWorld().Run();
             new DataTypes().Run();
 
-            var number = int.Parse(ReadLine().Trim());
+            //var number = int.Parse(ReadLine().Trim());
 
-            var binaryData = BinaryNumbers.DecimalToBinary(number);
-            var binaryValues = binaryData.Split('0');
-            var highestNumber = 0;
+            //var binaryData = BinaryNumbers.DecimalToBinary(number);
+            //var binaryValues = binaryData.Split('0');
+            //var highestNumber = 0;
 
-            //Method 1
-            highestNumber = binaryValues.OrderByDescending(s => s.Length).First().Length;
+            ////Method 1
+            //highestNumber = binaryValues.OrderByDescending(s => s.Length).First().Length;
 
-            //Method 2
-            foreach (var binary in binaryValues)
-            {
-                var value = binary.Length;
-                if(highestNumber < value)
-                {
-                    highestNumber = value;
-                }
-            }
+            ////Method 2
+            //foreach (var binary in binaryValues)
+            //{
+            //    var value = binary.Length;
+            //    if(highestNumber < value)
+            //    {
+            //        highestNumber = value;
+            //    }
+            //}
             
-            WriteLine($"{highestNumber}");
+            //WriteLine($"{highestNumber}");
 
             WriteLine("\n\nPress any key.");
             ReadKey();
