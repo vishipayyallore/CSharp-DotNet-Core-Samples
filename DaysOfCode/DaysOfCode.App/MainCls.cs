63f4aedd74fb026874d1e02056331d449daf423b
Viswanatha Swamy <vishipayyallore@yahoo.com>
2017-03-11 22:59:37 +0530

    .

--- DaysOfCode/DaysOfCode.App/MainCls.cs
+++ DaysOfCode/DaysOfCode.App/MainCls.cs
@@ -1,5 +1,4 @@
-﻿using Days.Programs;
-using System;
+﻿using System;
 using System.Reflection;
 using static System.Console;
 
@@ -19,29 +18,30 @@ namespace DaysOfCode.App
                 currentMethod.Invoke(Activator.CreateInstance(currentClass), null);
             }
             
-            //var number = int.Parse(ReadLine().Trim());
-
-            //var binaryData = BinaryNumbers.DecimalToBinary(number);
-            //var binaryValues = binaryData.Split('0');
-            //var highestNumber = 0;
-
-            ////Method 1
-            //highestNumber = binaryValues.OrderByDescending(s => s.Length).First().Length;
-
-            ////Method 2
-            //foreach (var binary in binaryValues)
-            //{
-            //    var value = binary.Length;
-            //    if(highestNumber < value)
-            //    {
-            //        highestNumber = value;
-            //    }
-            //}
-            
-            //WriteLine($"{highestNumber}");
-
             WriteLine("\n\nPress any key.");
             ReadKey();
         }
     }
 }
+
+//var number = int.Parse(ReadLine().Trim());
+
+//var binaryData = BinaryNumbers.DecimalToBinary(number);
+//var binaryValues = binaryData.Split('0');
+//var highestNumber = 0;
+
+////Method 1
+//highestNumber = binaryValues.OrderByDescending(s => s.Length).First().Length;
+
+////Method 2
+//foreach (var binary in binaryValues)
+//{
+//    var value = binary.Length;
+//    if(highestNumber < value)
+//    {
+//        highestNumber = value;
+//    }
+//}
+
+//WriteLine($"{highestNumber}");
+
