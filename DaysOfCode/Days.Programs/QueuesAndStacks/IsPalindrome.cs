8edac22f2dd0df0cacc358e3a4b4d463f72c0401
Viswanatha Swamy <vishipayyallore@yahoo.com>
2017-04-01 21:53:53 +0530

    .

---
+++ DaysOfCode/Days.Programs/QueuesAndStacks/IsPalindrome.cs
@@ -0,0 +1,44 @@
+﻿using Days.CoreLibrary;
+using System;
+
+namespace Days.Programs.QueuesAndStacks
+{
+    public class IsPalindrome : IProgram
+    {
+        public void Run()
+        {
+            var stackNQueue = new StackNQueue();
+            bool isPalindrome = true;
+
+            foreach (char c in Console.ReadLine().Trim())
+            {
+                stackNQueue.pushCharacter(c);
+                stackNQueue.enqueueCharacter(c);
+            }
+
+
+            // pop the top character from stack.
+            // dequeue the first character from queue.
+            // compare both the characters.
+            for (int i = 0; i < s.Length / 2; i++)
+            {
+                if (obj.popCharacter() != obj.dequeueCharacter())
+                {
+                    isPalindrome = false;
+
+                    break;
+                }
+            }
+
+            // finally print whether string s is palindrome or not.
+            if (isPalindrome)
+            {
+                Console.Write("The word, {0}, is a palindrome.", s);
+            }
+            else
+            {
+                Console.Write("The word, {0}, is not a palindrome.", s);
+            }
+        }
+    }
+}
---
+++ DaysOfCode/Days.Programs/QueuesAndStacks/StackNQueue.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+
+namespace Days.Programs.QueuesAndStacks
+{
+
+    public class StackNQueue
+    {
+
+        Stack<char> stackData = new Stack<char>();
+        Queue<char> queueData = new Queue<char>();
+
+        public void PushCharacter(char ch)
+        {
+            stackData.Push(ch);
+        }
+
+        public void EnqueueCharacter(char ch)
+        {
+            queueData.Enqueue(ch);
+        }
+
+        public char PopCharacter()
+        {
+            return stackData.Pop();
+        }
+
+        public char DequeueCharacter()
+        {
+            return queueData.Dequeue();
+        }
+
+    }
+
+}
