f8bc48c4f8e0331fc163ffaff375ac16939cb7fb
Viswanatha Swamy <vishipayyallore@yahoo.com>
2017-04-04 14:07:08 +0530

    .

---
+++ DaysOfCode/Days.Programs/Generics/ArrayPrinter.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Days.Programs.Generics
+{
+    public class ArrayPrinter
+    {
+        public void PrintArray<T>(T arrayElements)
+        {
+            foreach(var current in arrayElements.)
+            {
+
+            }
+        }
+    }
+}
