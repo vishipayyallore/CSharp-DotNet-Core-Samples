9feb3fc8af3325e3990300d9d0354a0cf5e554e4
Viswanatha Swamy <vishipayyallore@yahoo.com>
2017-04-05 15:02:22 +0530

    .

---
+++ DaysOfCode/Days.Programs/HowToGenerics.cs
@@ -0,0 +1,15 @@
+﻿using Days.CoreLibrary;
+using Days.Programs.Generics;
+
+namespace Days.Programs
+{
+    public class HowToGenerics : IProgram
+    {
+        public void Run()
+        {
+            ArrayPrinter.PrintArray(new int[] { 1, 3, 4 });
+            ArrayPrinter.PrintArray(new string[] { "Shiva", "Sai", "Mathews" });
+            ArrayPrinter.PrintArray(new char[] { 'A', 'B', 'C' });
+        }
+    }
+}
