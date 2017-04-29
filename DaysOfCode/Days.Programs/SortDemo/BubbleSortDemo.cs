6704fafb46087055760fcf3fb857590a2070dc25
Viswanatha Swamy <vishipayyallore@yahoo.com>
2017-04-29 23:32:35 +0530

    .

--- DaysOfCode/Days.Programs/SortDemo/BubbleSortDemo.cs
+++ DaysOfCode/Days.Programs/SortDemo/BubbleSortDemo.cs
@@ -1,7 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using Days.CoreLibrary;
 using System.Linq;
-using System.Threading.Tasks;
+using static System.Console;
 
 namespace Days.Programs.SortDemo
 {
@@ -9,8 +8,36 @@ namespace Days.Programs.SortDemo
     {
         public void Run()
         {
-            var numbers = new int[] { 2, 4, 5 };
+            var numbers = ReadLine().Trim().Split(' ').Select(int.Parse).ToArray();
+            var currentValue = 0;
+            var totalSwaps = 0;
 
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                int numberOfSwaps = 0;
+
+                for (int j = 0; j < numbers.Length - 1; j++)
+                {
+                    if (numbers[j] > numbers[j + 1])
+                    {
+                        //swap(a[j], a[j + 1]);
+                        currentValue = numbers[j];
+                        numbers[j] = numbers[j + 1];
+                        numbers[j + 1] = currentValue;
+                        numberOfSwaps++;
+                    }
+                }
+
+                totalSwaps += numberOfSwaps;
+                if (numberOfSwaps == 0)
+                {
+                    break;
+                }
+            }
+
+            WriteLine($"Array is sorted in {totalSwaps} swaps.");
+            WriteLine($"First Element: {numbers.First()}");
+            WriteLine($"Last Element: {numbers.Last()}");
         }
     }
 }
