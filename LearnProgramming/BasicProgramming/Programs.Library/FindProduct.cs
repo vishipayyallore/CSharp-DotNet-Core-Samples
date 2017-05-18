20a4036f73a4c5eb4d7df63c0aae9d306d476abb
Viswanatha Swamy <vishipayyallore@yahoo.com>
2017-05-18 22:45:19 +0530

    .

---
+++ LearnProgramming/BasicProgramming/Programs.Library/FindProduct.cs
@@ -0,0 +1,25 @@
+﻿using BasicProgramming.CoreLibrary;
+using System;
+using System.Linq;
+using static System.Console;
+
+namespace Programs.Library
+{
+    class FindProduct : IProgram
+    {
+        #region
+        public void Run()
+        {
+            var numberOfElements = int.Parse(ReadLine().Trim());
+            var arrayValues = ReadLine().Trim().Split(' ').Select(int.Parse).ToArray();
+            var answer = 1.0;
+            var theValue = Math.Pow(10, 9) + 7;
+            for (var iCtr = 0; iCtr < numberOfElements; iCtr++)
+            {
+                answer = (answer * arrayValues[iCtr]) % theValue;
+            }
+            WriteLine(answer);
+        }
+        #endregion
+    }
+}
