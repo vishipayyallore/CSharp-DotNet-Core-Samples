b1f9b395b08cf9ee76c63aa879ec89229a9b961f
Viswanatha Swamy <vishipayyallore@yahoo.com>
2017-05-22 22:23:59 +0530

    .

--- LearnProgramming/BasicProgramming/Programs.Library/CountDivisors.cs
+++ LearnProgramming/BasicProgramming/Programs.Library/CountDivisors.cs
@@ -1,11 +1,23 @@
-﻿using BasicProgramming.CoreLibrary;
-using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using System.Linq;
+using BasicProgramming.CoreLibrary;
+using static System.Console;
 
 namespace Programs.Library
 {
     class CountDivisors : IProgram
     {
+        public void Run()
+        {
+            var arrayValues = ReadLine().Trim().Split(' ').Select(int.Parse).ToArray();
+            var count = 0;
+            for (var iCtr = arrayValues[0]; iCtr <= arrayValues[1]; iCtr++)
+            {
+                if (iCtr % arrayValues[2] == 0)
+                {
+                    count++;
+                }
+            }
+            WriteLine(count);
+        }
     }
 }
