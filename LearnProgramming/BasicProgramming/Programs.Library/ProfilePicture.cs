2321250a2d120035b8402bbf866cdaf994b2d07a
Viswanatha Swamy <vishipayyallore@yahoo.com>
2017-05-23 22:48:45 +0530

    .

--- LearnProgramming/BasicProgramming/Programs.Library/ProfilePicture.cs
+++ LearnProgramming/BasicProgramming/Programs.Library/ProfilePicture.cs
@@ -1,10 +1,24 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using BasicProgramming.CoreLibrary;
+using static System.Console;
 
 namespace Programs.Library
 {
-    class ProfilePicture
+    class ProfilePicture : IProgram
     {
+        public void Run()
+        {
+            var pictureLength = int.Parse(ReadLine().Trim());
+            var numberOfPictures = int.Parse(ReadLine().Trim());
+            for (var iCtr = 0; iCtr < numberOfPictures; iCtr++)
+            {
+                var pictureDimension = ReadLine().Trim().Split(' ').Select(int.Parse).ToArray();
+                if (pictureDimension[0] < pictureLength || pictureDimension[1] < pictureLength)
+                {
+                    WriteLine("UPLOAD ANOTHER");
+                    continue;
+                }
+                WriteLine(((pictureDimension[0] == pictureLength && pictureDimension[1] == pictureLength) || (pictureDimension[0] == pictureDimension[1])) ? "ACCEPTED" : "CROP IT");
+            }
+        }
     }
 }
