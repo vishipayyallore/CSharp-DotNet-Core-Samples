81ceae2b60ffce757e968c2e011224b3ac0b8042
Viswanatha Swamy <vishipayyallore@yahoo.com>
2017-05-24 23:04:58 +0530

    .

---
+++ LearnProgramming/BasicProgramming/Programs.Library/PrimeNumber.cs
@@ -0,0 +1,38 @@
+﻿using BasicProgramming.CoreLibrary;
+using static System.Console;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Programs.Library
+{
+    class PrimeNumber : IProgram
+    {
+        private static bool IsPrime(int number)
+        {
+            var iCtr = 2;
+            while (iCtr < number)
+            {
+                if (number % iCtr == 0)
+                {
+                    break;
+                }
+                iCtr++;
+            }
+            return (iCtr == number) ? true : false;
+        }
+
+        public void Run()
+        {
+            var number = int.Parse(ReadLine().Trim());
+            var iCtr = 2;
+            while (iCtr <= number)
+            {
+                if (IsPrime(iCtr))
+                {
+                    Write($"{iCtr} ");
+                }
+                iCtr++;
+            }
+        }
+    }
+}
