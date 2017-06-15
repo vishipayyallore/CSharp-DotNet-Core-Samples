1f1f193e2cea81bfe1863b9e6c38bb03be665a0c
Viswanatha Swamy <vishipayyallore@yahoo.com>
2017-06-15 22:40:09 +0530

    .

---
+++ LearnProgramming/BasicProgramming/Programs.Library/LifeUniverseEverything.cs
@@ -0,0 +1,22 @@
+﻿using BasicProgramming.CoreLibrary;
+using static System.Console;
+
+namespace Programs.Library
+{
+    class LifeUniverseEverything : IProgram
+    {
+        public void Run()
+        {
+            WriteLine("Stop processing input after reading in the number 42");
+            do
+            {
+                var number = int.Parse(ReadLine().Trim());
+                if (number == 42)
+                {
+                    break;
+                }
+                WriteLine(number);
+            } while (true);
+        }
+    }
+}
