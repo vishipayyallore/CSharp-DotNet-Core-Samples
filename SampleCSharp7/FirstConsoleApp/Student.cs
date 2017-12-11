bcf892a4c13290b5c9210d8821e25b1fef63fccf
Viswanatha Swamy <vishipayyallore@yahoo.com>
2017-12-12 00:38:44 +0530

    Added Student file

---
+++ SampleCSharp7/FirstConsoleApp/Student.cs
@@ -0,0 +1,14 @@
+﻿namespace FirstConsoleApp
+{
+    public class Student
+    {
+        public (int average, int studentCount) GetAverageAndCount(int[] scores)
+        {
+            var result = (average:0, studentCount:0);
+
+            return result;
+        }
+
+    }
+
+}
