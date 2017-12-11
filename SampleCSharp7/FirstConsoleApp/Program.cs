5da864e386d5d4b9ed2941fd758d73e221d2bb1f
Viswanatha Swamy <vishipayyallore@yahoo.com>
2017-12-12 00:23:20 +0530

    Added simple console application

---
+++ SampleCSharp7/FirstConsoleApp/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using static System.Console;
+
+namespace FirstConsoleApp
+{
+    static class Program
+    {
+        private static void Main()
+        {
+            ValueTuple _tupleVariable;
+            WriteLine("Hello World!");
+
+            WriteLine("\n\nPress any key...");
+            ReadKey();
+        }
+    }
+}
