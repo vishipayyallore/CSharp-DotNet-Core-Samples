36a21271c5afcfb412a21dec0b222261020d70c0
Viswanatha Swamy <vishipayyallore@yahoo.com>
2017-12-12 00:42:05 +0530

    Basic coding

--- SampleCSharp7/FirstConsoleApp/Program.cs
+++ SampleCSharp7/FirstConsoleApp/Program.cs
@@ -7,8 +7,12 @@ namespace FirstConsoleApp
     {
         private static void Main()
         {
-            ValueTuple _tupleVariable;
-            WriteLine("Hello World!");
+            ForegroundColor = ConsoleColor.Yellow;
+            var student = new Student();
+            var scores = new[] {17, 46, 39, 62, 81, 79, 52, 24};
+
+            var studentsResult = student.GetAverageAndCount(scores);
+            WriteLine($"Average was {studentsResult.average} across {studentsResult.studentCount}");
 
             WriteLine("\n\nPress any key...");
             ReadKey();
