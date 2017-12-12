f0428dff35ff6a505ccf4f69db147288ea5093c0
Viswanatha Swamy <vishipayyallore@yahoo.com>
2017-12-13 01:25:19 +0530

    slipt the output variables from method.

--- SampleCSharp7/FirstConsoleApp/Program.cs
+++ SampleCSharp7/FirstConsoleApp/Program.cs
@@ -11,8 +11,8 @@ namespace FirstConsoleApp
             var student = new Student();
             var scores = new[] {17, 46, 39, 62, 81, 79, 52, 24};
 
-            var studentsResult = student.GetAverageAndCount(scores);
-            WriteLine($"Average was {studentsResult.average} across {studentsResult.studentCount}");
+            var (average, studentCount) = student.GetAverageAndCount(scores);
+            WriteLine($"Average was {average} across {studentCount}");
 
             WriteLine("\n\nPress any key...");
             ReadKey();
