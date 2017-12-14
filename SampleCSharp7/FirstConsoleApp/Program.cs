44e8530c8cd1b652c8a308d1e350862beba420d9
Viswanatha Swamy <vishipayyallore@yahoo.com>
2017-12-14 23:51:29 +0530

    removed the unused variable.

--- SampleCSharp7/FirstConsoleApp/Program.cs
+++ SampleCSharp7/FirstConsoleApp/Program.cs
@@ -10,9 +10,9 @@ namespace FirstConsoleApp
             ForegroundColor = ConsoleColor.Yellow;
             var student = new Student();
             var scores = new[] {17, 46, 39, 62, 81, 79, 52, 24};
-            const int threshold = 51;
+            const int threshold = 40;
 
-            var (average, studentCount, belowAverage) = student.GetAverageAndCount(scores, threshold);
+            var (average, studentCount, _) = student.GetAverageAndCount(scores, threshold);
             WriteLine($"Average was {average} across {studentCount} students. Below Average: {(average < threshold ? " Class score below average." : " Class score above average.")}");
 
             WriteLine("\n\nPress any key...");
