3078c5e29d51ae1b4adebf64ad5cb9e2edc7c6e6
Viswanatha Swamy <vishipayyallore@yahoo.com>
2017-12-14 23:17:34 +0530

    Adding Static class

---
+++ SampleCSharp7/FirstConsoleApp/ExtensionMethods.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FirstConsoleApp
+{
+    public static class ExtensionMethods
+    {
+        public static bool CheckIfBelowAverage()
+        {
+            return true;
+        }
+
+    }
+}
