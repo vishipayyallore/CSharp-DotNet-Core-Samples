f29430b767cabcd5ab20109b7b96799c63b8a99a
Viswanatha Swamy <vishipayyallore@yahoo.com>
2017-12-16 13:48:58 +0530

    Added student class.

---
+++ SampleCSharp7/FirstConsoleApp/Student.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FirstConsoleApp
+{
+    public class Student
+    {
+        public string Name { get; set; }
+        public string LastName { get; set; }
+        public List<int> CouseCodes { get; set; }
+    }
+}
