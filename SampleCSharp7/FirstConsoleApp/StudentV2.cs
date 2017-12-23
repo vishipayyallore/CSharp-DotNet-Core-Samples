beb5a16d33a3cc83ea1e40588f528d0756836f15
Viswanatha Swamy <vishipayyallore@yahoo.com>
2017-12-23 20:24:16 +0530

    Student V2

---
+++ SampleCSharp7/FirstConsoleApp/StudentV2.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+
+namespace FirstConsoleApp
+{
+    public class StudentV2
+    {
+
+        public StudentV2(string studentNumber)
+        {
+            (Name, LastName) = GetStudentDetails(studentNumber);
+        }
+
+        public string Name { get; private set; }
+        public string LastName { get; private set; }
+        public List<int> CourseCodes { get; private set; }
+
+        public void Deconstruct(out string name, out string lastName)
+        {
+            name = Name;
+            lastName = LastName;
+        }
+
+        private (string name, string surname) GetStudentDetails(string studentNumber)
+        {
+            var studentDetails = (name: "Dirk", surname: "Strauss");
+            return studentDetails;
+        }
+
+    }
+}
