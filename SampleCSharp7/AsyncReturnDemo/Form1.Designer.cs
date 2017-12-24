175ca58fd7cf9829944a4ce544c577a3d130eb2d
Viswanatha Swamy <vishipayyallore@yahoo.com>
2017-12-24 21:39:10 +0530

    new

---
+++ SampleCSharp7/AsyncReturnDemo/Form1.Designer.cs
@@ -0,0 +1,91 @@
+﻿namespace AsyncReturnDemo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.btnTestAsync = new System.Windows.Forms.Button();
+            this.lblTimer = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // btnTestAsync
+            // 
+            this.btnTestAsync.Location = new System.Drawing.Point(12, 13);
+            this.btnTestAsync.Name = "btnTestAsync";
+            this.btnTestAsync.Size = new System.Drawing.Size(115, 39);
+            this.btnTestAsync.TabIndex = 0;
+            this.btnTestAsync.Text = "Test Async";
+            this.btnTestAsync.UseVisualStyleBackColor = true;
+            // 
+            // lblTimer
+            // 
+            this.lblTimer.AutoSize = true;
+            this.lblTimer.Location = new System.Drawing.Point(12, 66);
+            this.lblTimer.Name = "lblTimer";
+            this.lblTimer.Size = new System.Drawing.Size(51, 20);
+            this.lblTimer.TabIndex = 1;
+            this.lblTimer.Text = "label1";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(150, 25);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(381, 26);
+            this.textBox1.TabIndex = 2;
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(559, 149);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.lblTimer);
+            this.Controls.Add(this.btnTestAsync);
+            this.Name = "Form1";
+            this.Text = "Async Return Demo";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnTestAsync;
+        private System.Windows.Forms.Label lblTimer;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ SampleCSharp7/AsyncReturnDemo/Form1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AsyncReturnDemo
+
+{
+    public partial class Form1 : Form
+    {
+        private double timerTtl = 10.0D;
+        private DateTime timeToLive;
+        private int cacheValue;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            lblTimer.Text = $"Timer TTL {timerTtl} sec (STOPPED)";
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (timerTtl == 0)
+            {
+                timerTtl = 5;
+            }
+            else
+            {
+                timerTtl -= 1;
+            }
+            lblTimer.Text = $"Timer TTL {timerTtl} sec (RUNNING)";
+        }
+
+
+        public async Task<int> GetValue()
+        {
+            await Task.Delay(1000);
+
+            var random = new Random();
+            cacheValue = random.Next();
+            timeToLive = DateTime.Now.AddSeconds(timerTtl);
+
+            timer1.Start();
+            return cacheValue;
+        }
+
+        public ValueTask<int> LoadReadCache(out bool isValueCached)
+        {
+            if (timeToLive < DateTime.Now)
+            {
+                isValueCached = false;
+                return new ValueTask<int>(GetValue());
+            }
+            isValueCached = true;
+            return new ValueTask<int>(cacheValue);
+        }
+        
+    }
+}
