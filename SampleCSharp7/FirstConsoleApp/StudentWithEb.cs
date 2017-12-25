820ed0b191dc293d1e781bfe66bccd752819b106
Viswanatha Swamy <vishipayyallore@yahoo.com>
2017-12-25 14:16:31 +0530

    Expression body demo

---
+++ SampleCSharp7/FirstConsoleApp/StudentWithEb.cs
@@ -0,0 +1,19 @@
+﻿using static System.Console;
+
+namespace FirstConsoleApp
+{
+    public class StudentWithEb
+    {
+        private int _age;
+
+        public StudentWithEb(int age) => _age = age;
+
+        public int Age
+        {
+            get => _age;
+            set => _age = value;
+        }
+
+        ~StudentWithEb() => WriteLine(" Release unmanaged code");
+    }
+}
---
+++ SampleCSharp7/FirstConsoleApp/StudentWithoutEb.cs
@@ -0,0 +1,27 @@
+﻿using static System.Console;
+
+namespace FirstConsoleApp
+{
+    public class StudentWithoutEb
+    {
+        private int _age;
+
+        public StudentWithoutEb(int age)
+        {
+            _age = age;
+        }
+
+        public int Age
+        {
+            get { return _age; }
+            set { _age = value; }
+        }
+
+        //Finalizers
+        ~StudentWithoutEb()
+        {
+            WriteLine(" Release unmanaged code");
+        }
+
+    }
+}
