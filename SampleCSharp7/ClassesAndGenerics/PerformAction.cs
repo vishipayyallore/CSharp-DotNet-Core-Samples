094568c124ab9235cdfeb172d3079fc60a15f17c
Viswanatha Swamy <vishipayyallore@yahoo.com>
2017-12-27 23:44:01 +0530

    Generic class demo

---
+++ SampleCSharp7/ClassesAndGenerics/PerformAction.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassesAndGenerics
+{
+    public class PerformAction<T>
+    {
+        private T _value;
+
+    }
+}
