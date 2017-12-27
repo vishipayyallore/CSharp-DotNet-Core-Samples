8a8ae5f11d4d80186a65157828361cfe0e84c87d
Viswanatha Swamy <vishipayyallore@yahoo.com>
2017-12-27 23:46:49 +0530

    basic method.

--- SampleCSharp7/ClassesAndGenerics/PerformAction.cs
+++ SampleCSharp7/ClassesAndGenerics/PerformAction.cs
@@ -1,12 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using static System.Console;
 
 namespace ClassesAndGenerics
 {
     public class PerformAction<T>
     {
-        private T _value;
+        private readonly T _value;
 
+        public PerformAction(T value) => _value = value;
+
+        public void IdentityDataType()
+        {
+            WriteLine($" The data type of the supplied variable is {_value.GetType()}");
+        }
     }
 }
