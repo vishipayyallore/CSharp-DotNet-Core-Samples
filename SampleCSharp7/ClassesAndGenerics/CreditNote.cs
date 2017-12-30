5e1b3d08de6f6b6fde87659ed3ce0b2b0a3f7354
Viswanatha Swamy <vishipayyallore@yahoo.com>
2017-12-30 23:03:42 +0530

    Removed unused imports.

--- SampleCSharp7/ClassesAndGenerics/CreditNote.cs
+++ SampleCSharp7/ClassesAndGenerics/CreditNote.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace ClassesAndGenerics
+﻿namespace ClassesAndGenerics
 {
     public class CreditNote : AcmeObject
     {
