4ea96bd4dc17dcc7b0e45c8bc1b9c5c73085fcc7
Viswanatha Swamy <vishipayyallore@yahoo.com>
2018-01-01 15:46:07 +0530

    basic version of the class.

---
+++ SampleCSharp7/ClassesAndGenerics/LaunchSuttle.cs
@@ -0,0 +1,22 @@
+﻿namespace ClassesAndGenerics
+{
+    public class LaunchSuttle
+    {
+        private double _engineThrust;
+        private double _totalShuttleMass;
+        private double _localGravitationalAccelaration;
+        private double _universalGravitationalConstant;
+
+        private const double EarthGravity = 9.81;
+        private const double MoonGravity = 1.63;
+        private const double MarsGravity = 3.75;
+
+        public LaunchSuttle(double engineThrust, double totalShuttleMass, double localGravitationalAccelaration)
+        {
+            _engineThrust = engineThrust;
+            _totalShuttleMass = totalShuttleMass;
+            _localGravitationalAccelaration = localGravitationalAccelaration;
+        }
+
+    }
+}
