27a7107976508634d82a45975f61e57f325bab85
Viswanatha Swamy <vishipayyallore@yahoo.com>
2018-01-04 21:50:55 +0530

    two overloaded constructor [static Polymophism]

--- SampleCSharp7/Polymorphism.Lib/NasaShuttle.cs
+++ SampleCSharp7/Polymorphism.Lib/NasaShuttle.cs
@@ -2,9 +2,22 @@
 {
     public class NasaShuttle : Shuttle
     {
+
+        public NasaShuttle(double engineThrust, double totalShuttleMass, double gravitationalAcceleration)
+        {
+            
+        }
+
+        public NasaShuttle(double engineThrust, double totalShuttleMass, 
+            double planetMass, double planetRadius)
+        {
+            
+        }
+
         public override double Twr()
         {
             throw new System.NotImplementedException();
         }
+
     }
 }
