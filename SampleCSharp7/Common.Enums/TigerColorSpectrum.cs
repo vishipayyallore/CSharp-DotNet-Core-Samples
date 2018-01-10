c997b1237264f72a386b427794f08c1c8612a5c4
Viswanatha Swamy <vishipayyallore@yahoo.com>
2018-01-10 17:23:49 +0530

    New library to hold all the enums

---
+++ SampleCSharp7/Common.Enums/TigerColorSpectrum.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Common.Enums
+{
+    public enum TigerColorSpectrum
+    {
+        Orange,
+        White,
+        Gold,
+        Blue,
+        Black
+    }
+
+    public enum Planet
+    {
+        Earth,
+        Moon,
+        Mars
+    }
+
+    public enum LionColorSpectrum
+    {
+        Brown,
+        White
+    }
+
+    public enum CatType
+    {
+        Lion,
+        Tiger,
+        Unknown
+    }
+}
