8986b46d15b25a66eaa2f900389f61cb00c2bb5c
Viswanatha Swamy <vishipayyallore@yahoo.com>
2018-08-10 23:44:35 +0530

    Simple Func<>

--- Samples.CSharp7/Source/CSharp7.DemoApp/Program.cs
+++ Samples.CSharp7/Source/CSharp7.DemoApp/Program.cs
@@ -1,15 +1,29 @@
 ﻿using System;
+using System.Collections.Generic;
 using static System.Console;
 
 namespace CSharp7.DemoApp
 {
     class Program
     {
+
+        static void SelectNumbers(List<int> numbers, Func<int, bool> filterCriteria)
+        {
+            foreach(var number in numbers)
+            {
+                var output = filterCriteria(number);
+                WriteLine($"{number} = {output} ");
+            }
+        }
+
         static void Main(string[] args)
         {
+            var numbers = new List<int> { 3, 8, 4, 6, 1, 7, 9, 2, 4, 8, 0 };
 
             ForegroundColor = ConsoleColor.Yellow;
             WriteLine("Hello World!");
+
+            SelectNumbers(numbers, x => (x%2 == 0));
 
             WriteLine("\n\nPress any key ...");
             Read();
