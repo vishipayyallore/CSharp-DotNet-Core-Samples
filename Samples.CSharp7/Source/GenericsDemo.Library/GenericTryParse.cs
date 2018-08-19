0eb77f6a351b31646d02a3158ddfd18064dcf3d4
Viswanatha Swamy <vishipayyallore@yahoo.com>
2018-08-19 22:45:15 +0530

    Working on TryParse

---
+++ Samples.CSharp7/Source/GenericsDemo.Library/GenericTryParse.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace GenericsDemo.Library
+{
+
+    public class GenericTryParse
+    {
+
+        public int TryParse(string value)
+        {
+            // Inline out variable declaration
+            int.TryParse(value, out int parsedValue);
+            return parsedValue;
+        }
+
+    }
+
+}
