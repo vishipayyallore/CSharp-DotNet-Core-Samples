46b8d8f1b6977bfce4a45a533f4d99706d6cb66a
Viswanatha Swamy <vishipayyallore@yahoo.com>
2018-08-20 22:54:56 +0530

    Simple Test

--- Samples.CSharp7/Tests/Functional.Programs.Tests/FilterDataTests.cs
+++ Samples.CSharp7/Tests/Functional.Programs.Tests/FilterDataTests.cs
@@ -1,7 +1,9 @@
 using CSharp7.Demo.Common;
 using Functional.Programs.Library;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace Functional.Programs.Tests
 {
@@ -30,6 +32,27 @@ namespace Functional.Programs.Tests
             var oddNumbers = _filterData.SelectValues(_numbers, e => e % 2 == 1);
 
             Assert.AreEqual(5, oddNumbers.Count);
+        }
+
+
+        [TestMethod]
+        public void When_SelectValuesWithOrderBy_IsPassed_ValidData()
+        {
+            var evenNumbers = _filterData.SelectValuesWithOrderBy(_numbers, e => e % 2 == 0, e => e);
+
+            Assert.AreEqual(5, evenNumbers.Count);
+            Assert.IsTrue(IsSorted(evenNumbers));
+        }
+
+        private static bool IsSorted<T>(IEnumerable<T> list) where T : IComparable<T>
+        {
+            var y = list.First();
+            return list.Skip(1).All(x =>
+            {
+                bool b = y.CompareTo(x) < 0;
+                y = x;
+                return b;
+            });
         }
 
     }
