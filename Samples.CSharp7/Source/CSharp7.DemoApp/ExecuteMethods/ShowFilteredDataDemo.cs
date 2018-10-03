3f62d2d45be1424b9a3ed6f1803c19856e0611c8
Viswanatha Swamy <vishipayyallore@yahoo.com>
2018-10-03 20:56:06 +0530

    Showing Simple Filtered Data Demo

--- Samples.CSharp7/Source/CSharp7.DemoApp/ExecuteMethods/ShowFilteredDataDemo.cs
+++ Samples.CSharp7/Source/CSharp7.DemoApp/ExecuteMethods/ShowFilteredDataDemo.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Functional.Programs.Library;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -6,5 +7,38 @@ namespace CSharp7.DemoApp.ExecuteMethods
 {
     public class ShowFilteredDataDemo
     {
+        private List<int> numbers = new List<int> { 3, 8, 4, 6, 1, 7, 5, 2, 9, 10 };
+        private readonly FilterData filterDataObject = new FilterData();
+        private readonly ExecuteMethodsHelper _executeMethodsHelper;
+
+        public ShowFilteredDataDemo()
+        {
+            _executeMethodsHelper = new ExecuteMethodsHelper();
+        }
+
+
+        public ShowFilteredDataDemo ShowSimpleFilteredDataDemo()
+        {
+            _executeMethodsHelper.ShowHeader($"ShowFilteredDataDemo::ShowSimpleFilteredDataDemo ----- -----");
+
+            filterDataObject.CalculateAndShowValues(numbers, false, x => x + 2);
+            filterDataObject.CalculateAndShowValues(numbers, false, x => x * x);
+            filterDataObject.CalculateAndShowValues(numbers, false, x => (x * x * x));
+            
+            // Odd Numbers
+            filterDataObject.CalculateAndShowValues(numbers, true, x =>
+            {
+                return (x % 2 == 1) ? x : 0;
+            });
+            
+            // Even Numbers
+            filterDataObject.CalculateAndShowValues(numbers, true, x =>
+            {
+                return (x % 2 == 0) ? x : 0;
+            });
+
+            return this;
+        }
+
     }
 }
