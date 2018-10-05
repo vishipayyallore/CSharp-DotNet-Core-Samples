547f24841132ecd9124d7c99296a5d156705c1f0
Viswanatha Swamy <vishipayyallore@yahoo.com>
2018-10-05 18:11:29 +0530

    PlusMinus logic program

---
+++ Samples.CSharp7/CSharp7.Logic.Programs/PlusMinus.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using static System.Console;
+
+namespace CSharp7.Logic.Programs
+{
+
+    public class PlusMinus
+    {
+
+        static List<int> SelectArrayValues(int[] numbers, Func<int, bool> filterCriteria)
+        {
+            return numbers.Where(filterCriteria)
+                    .ToList();
+        }
+
+        public void Execute()
+        {
+            var numberOfItems = int.Parse(ReadLine().Trim());
+            var arrayValues = ReadLine().Trim().Split(' ').Select(int.Parse).ToArray();
+            var values = SelectArrayValues(arrayValues, e => e > 0);
+            WriteLine(string.Format("{0:F6}", (values.Count / (numberOfItems * 1.0))));
+
+            values = SelectArrayValues(arrayValues, e => e < 0);
+            WriteLine(string.Format("{0:F6}", (values.Count / (numberOfItems * 1.0))));
+
+            values = SelectArrayValues(arrayValues, e => e == 0);
+            WriteLine(string.Format("{0:F6}", (values.Count / (numberOfItems * 1.0))));
+        }
+
+    }
+
+}
