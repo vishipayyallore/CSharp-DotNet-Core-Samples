51416a37747d0c63a761f2eea510f53203794eec
Viswanatha Swamy <vishipayyallore@yahoo.com>
2018-10-05 18:14:32 +0530

    Sample Sort Program

---
+++ Samples.CSharp7/CSharp7.Logic.Programs/SampleSort.cs
@@ -0,0 +1,18 @@
+﻿using System.Linq;
+using static System.Console;
+
+namespace CSharp7.Logic.Programs
+{
+    public class SampleSort
+    {
+
+        public void Execute()
+        {
+            var numberOfItems = int.Parse(ReadLine().Trim());
+            var arrayValues = ReadLine().Trim().Split(' ').Select(int.Parse).ToArray();
+
+        }
+
+    }
+
+}
