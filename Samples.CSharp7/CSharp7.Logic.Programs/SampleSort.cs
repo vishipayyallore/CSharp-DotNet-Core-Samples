532358a45137ff47f4362929d2cd1248bd9ef11b
Viswanatha Swamy <vishipayyallore@yahoo.com>
2018-10-07 20:59:05 +0530

    SimpleSortingProgram

--- Samples.CSharp7/CSharp7.Logic.Programs/SampleSort.cs
+++ Samples.CSharp7/CSharp7.Logic.Programs/SampleSort.cs
@@ -1,4 +1,5 @@
-﻿using System.Linq;
+﻿using System.Collections.Generic;
+using System.Linq;
 using static System.Console;
 
 namespace CSharp7.Logic.Programs
@@ -8,9 +9,23 @@ namespace CSharp7.Logic.Programs
 
         public void Execute()
         {
+            Write($"Please Enter Number of Items: ");
             var numberOfItems = int.Parse(ReadLine().Trim());
+            Write($"Please Enter Array of Values: ");
             var arrayValues = ReadLine().Trim().Split(' ').Select(int.Parse).ToArray();
+            var numbersAndCount = new Dictionary<int, int>();
 
+            arrayValues.ToList().ForEach( number =>
+            { 
+                if(numbersAndCount.ContainsKey(number))
+                {
+                    numbersAndCount[number] += 1;
+                }
+                else
+                {
+                    numbersAndCount.Add(number, 1);
+                }
+            });
         }
 
     }
