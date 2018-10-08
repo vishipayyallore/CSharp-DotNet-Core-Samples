88959e32f275971d27d60c7a8558d5f698e8ae5e
Viswanatha Swamy <vishipayyallore@yahoo.com>
2018-10-08 08:56:38 +0530

    ..

--- Samples.CSharp7/CSharp7.Logic.Programs/SockMerchant.cs
+++ Samples.CSharp7/CSharp7.Logic.Programs/SockMerchant.cs
@@ -1,11 +1,18 @@
-﻿namespace CSharp7.Logic.Programs
+﻿using System.Linq;
+using static System.Console;
+
+namespace CSharp7.Logic.Programs
 {
     public class SockMerchant
     {
 
         public void Execute()
         {
-
+            WriteLine($"Sample Sort Program!");
+            Write($"Please Enter Number of Items: ");
+            var numberOfItems = int.Parse(ReadLine().Trim());
+            Write($"Please Enter Array of Values: ");
+            var arrayValues = ReadLine().Trim().Split(' ').Select(int.Parse).ToArray();
         }
 
     }
