122cd9679a3dda04402bef35aacffdc8be482a67
Viswanatha Swamy <vishipayyallore@yahoo.com>
2018-10-08 21:32:18 +0530

    Writing Logic

--- Samples.CSharp7/CSharp7.Logic.Programs/SockMerchant.cs
+++ Samples.CSharp7/CSharp7.Logic.Programs/SockMerchant.cs
@@ -8,11 +8,32 @@ namespace CSharp7.Logic.Programs
 
         public void Execute()
         {
-            WriteLine($"Sample Sort Program!");
+
+            int iCtr = 0;
+            int valleyCount = 0;
+            WriteLine($"Moutain - Valley Program!");
             Write($"Please Enter Number of Items: ");
             var numberOfItems = int.Parse(ReadLine().Trim());
             Write($"Please Enter Array of Values: ");
-            var arrayValues = ReadLine().Trim().Split(' ').Select(int.Parse).ToArray();
+            var valleyPath = ReadLine().Trim();
+
+            foreach(var path in valleyPath)
+            {
+                if(path == 'U')
+                {
+                    iCtr++;
+                }
+                else if(path == 'D')
+                {
+                    iCtr--;
+                }
+
+                if(iCtr == 0)
+                {
+                    valleyCount++;
+                }
+            }
+
         }
 
     }
