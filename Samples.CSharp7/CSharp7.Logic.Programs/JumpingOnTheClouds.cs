bc39fc0a4b70ed09a143f703e234a930878a25bf
Viswanatha Swamy <vishipayyallore@yahoo.com>
2018-10-22 09:32:27 +0530

    Moved the code into file

--- Samples.CSharp7/CSharp7.Logic.Programs/JumpingOnTheClouds.cs
+++ Samples.CSharp7/CSharp7.Logic.Programs/JumpingOnTheClouds.cs
@@ -1,11 +1,27 @@
-﻿namespace CSharp7.Logic.Programs
+﻿using static System.Console;
+
+namespace CSharp7.Logic.Programs
 {
     public class JumpingOnTheClouds
     {
 
-        public void Execute()
+        public void Execute(int[] arrayValues)
         {
+            var jumps = 0;
+            for (int iCtr = 0; iCtr < arrayValues.Length - 1; iCtr++)
+            {
+                if ((iCtr + 2 < arrayValues.Length) && arrayValues[iCtr + 2] == 0)
+                {
+                    iCtr += 1;
+                    jumps++;
+                }
+                else if (arrayValues[iCtr + 1] != 1 || arrayValues[iCtr + 2] != 1)
+                {
+                    jumps++;
+                }
+            }
 
+            WriteLine($"Number Of Jumps: {jumps}");
         }
 
     }
