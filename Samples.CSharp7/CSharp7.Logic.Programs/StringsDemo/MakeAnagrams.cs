b7411c2ea6130d437386348b20ea59f3d5aa4563
Viswanatha Swamy <vishipayyallore@yahoo.com>
2018-11-01 13:27:10 +0530

    Solved the problem

--- Samples.CSharp7/CSharp7.Logic.Programs/StringsDemo/MakeAnagrams.cs
+++ Samples.CSharp7/CSharp7.Logic.Programs/StringsDemo/MakeAnagrams.cs
@@ -1,26 +1,40 @@
-﻿namespace CSharp7.Logic.Programs.StringsDemo
+﻿using System;
+
+namespace CSharp7.Logic.Programs.StringsDemo
 {
     public class MakeAnagrams
     {
 
-        private int GetRemoveCount(string firstData, string secondData)
-        {
-            var count = 0;
-            foreach(var current in firstData)
-            {
-                if(secondData.IndexOf(current) < 0)
-                {
-                    count++;
-                }
-            }
+        private int[] charcterCount = new int[26];
 
-            return count;
-        }
+        //private void GetRemoveCount(string firstData, string secondData)
+        //{
+        //    var count = 0;
+        //    foreach(var current in firstData)
+        //    {
+        //        charcterCount[current - 'a']++;
+        //    }
+
+        //    return count;
+        //}
 
         public int Execute(string firstData, string secondData)
         {
-            var tobeRemoved = GetRemoveCount(firstData, secondData);
-            tobeRemoved += GetRemoveCount(secondData, firstData);
+            var tobeRemoved = 0;
+            foreach (var current in firstData)
+            {
+                charcterCount[current - 'a']++;
+            }
+
+            foreach (var current in secondData)
+            {
+                charcterCount[current - 'a']--;
+            }
+
+            foreach(var current in charcterCount)
+            {
+                tobeRemoved += Math.Abs(current);
+            }
 
             return tobeRemoved;
         }
