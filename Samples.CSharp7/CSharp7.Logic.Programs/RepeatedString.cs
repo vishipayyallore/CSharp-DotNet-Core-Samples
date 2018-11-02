5abc76ec80d348605a98d21b39426e51f476cc3a
Viswanatha Swamy <vishipayyallore@yahoo.com>
2018-11-02 08:59:06 +0530

    Moving the code into separate class.

---
+++ Samples.CSharp7/CSharp7.Logic.Programs/RepeatedString.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSharp7.Logic.Programs
+{
+    public class RepeatedString
+    {
+
+        public long Execute(string inputString, int n)
+        {
+            long count = 0;
+            long remaining = 0;
+
+            long toTraverse = (inputString.Length <= n) ? inputString.Length : n;
+            for (int iCtr = 0; iCtr < toTraverse; iCtr++)
+            {
+                if (inputString[iCtr] == 'a')
+                {
+                    count++;
+                }
+            }
+
+            if (inputString.Length <= n)
+            {
+                count = (n / inputString.Length) * count;
+                remaining = (n % inputString.Length);
+
+                for (int iCtr = 0; iCtr < remaining; iCtr++)
+                {
+                    if (inputString[iCtr] == 'a')
+                    {
+                        count++;
+                    }
+                }
+            }
+
+            return count;
+        }
+
+    }
+
+}
