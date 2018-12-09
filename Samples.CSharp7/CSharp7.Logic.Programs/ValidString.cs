c1c3bcf520ae0e5bb63f5fbcd56c8a024fdd4d19
Viswanatha Swamy <vishipayyallore@yahoo.com>
2018-12-09 22:12:16 +0530

    to do

--- Samples.CSharp7/CSharp7.Logic.Programs/ValidString.cs
+++ Samples.CSharp7/CSharp7.Logic.Programs/ValidString.cs
@@ -1,11 +1,41 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace CSharp7.Logic.Programs
+﻿namespace CSharp7.Logic.Programs
 {
     public class ValidString
     {
     }
 
 }
+
+//static String isValid(String s)
+//{
+//    final String GOOD = "YES";
+//    final String BAD = "NO";
+
+//    if (s.isEmpty()) return BAD;
+//    if (s.length() <= 3) return GOOD;
+
+//    int[] letters = new int[26];
+//    for (int i = 0; i < s.length(); i++)
+//    {
+//        letters[s.charAt(i) - 'a']++;
+//    }
+//    Arrays.sort(letters);
+//    int i = 0;
+//    while (letters[i] == 0)
+//    {
+//        i++;
+//    }
+//    //System.out.println(Arrays.toString(letters));
+//    int min = letters[i];   //the smallest frequency of some letter
+//    int max = letters[25]; // the largest frequency of some letter
+//    String ret = BAD;
+//    if (min == max) ret = GOOD;
+//    else
+//    {
+//        // remove one letter at higher frequency or the lower frequency 
+//        if (((max - min == 1) && (max > letters[24])) ||
+//            (min == 1) && (letters[i + 1] == max))
+//            ret = GOOD;
+//    }
+//    return ret;
+//}
