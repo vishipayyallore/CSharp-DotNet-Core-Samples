fbb3c0513c438134c267e3663edd286d6154298c
Viswanatha Swamy <vishipayyallore@yahoo.com>
2018-12-29 23:06:29 +0530

    Few formatting

--- Samples.CSharp7/CSharp7.Logic.Programs/SampleSort.cs
+++ Samples.CSharp7/CSharp7.Logic.Programs/SampleSort.cs
@@ -11,21 +11,21 @@ namespace CSharp7.Logic.Programs
         {
             var numbersAndCount = new Dictionary<int, int>();
 
-            arrayValues.ToList().ForEach( number =>
-            { 
-                if(numbersAndCount.ContainsKey(number))
-                {
-                    numbersAndCount[number] += 1;
-                }
-                else
-                {
-                    numbersAndCount.Add(number, 1);
-                }
-            });
+            arrayValues.ToList().ForEach(number =>
+           {
+               if (numbersAndCount.ContainsKey(number))
+               {
+                   numbersAndCount[number] += 1;
+               }
+               else
+               {
+                   numbersAndCount.Add(number, 1);
+               }
+           });
 
-            foreach(var number in numbersAndCount.Keys.OrderBy(x => x))
+            foreach (var number in numbersAndCount.Keys.OrderBy(x => x))
             {
-                for(int iCtr=1; iCtr <=numbersAndCount[number]; iCtr++)
+                for (int iCtr = 1; iCtr <= numbersAndCount[number]; iCtr++)
                 {
                     Write($"{number} ");
                 }
