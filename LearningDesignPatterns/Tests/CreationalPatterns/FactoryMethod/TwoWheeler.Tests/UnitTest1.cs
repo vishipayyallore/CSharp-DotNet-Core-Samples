6361320d7e8d4592077de77dcaf4d55f644554f4
Viswanatha Swamy <vishipayyallore@yahoo.com>
2019-10-09 23:17:02 +0530

    Added the Test case project

---
+++ LearningDesignPatterns/Tests/CreationalPatterns/FactoryMethod/TwoWheeler.Tests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TwoWheeler.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
