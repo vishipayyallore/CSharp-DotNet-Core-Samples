06c4bf36ab325b96c9190c91452fae089122b25d
Viswanatha Swamy <vishipayyallore@yahoo.com>
2019-10-17 07:55:54 +0530

    Simple Program

---
+++ LearningDesignPatterns/Source/Alogithms/Sample1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Numerics;
+
+namespace Sample1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BigInteger bigInteger = new BigInteger(double.MaxValue);
+
+            var output = GetFactorial(30);
+            Console.WriteLine($"Factorial: {output}");
+
+            Console.ReadLine();
+        }
+
+        private static BigInteger GetFactorial(int number)
+        {
+            if (number == 0)
+            {
+                return 1;
+            }
+
+            return number * GetFactorial(number - 1);
+        }
+
+    }
+
+}
