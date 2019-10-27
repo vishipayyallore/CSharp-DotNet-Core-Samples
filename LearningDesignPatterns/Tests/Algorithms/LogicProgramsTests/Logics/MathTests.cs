0b1d2ceb2048e33dd6f8c03a26f0d504772dbf7d
Viswanatha Swamy <vishipayyallore@yahoo.com>
2019-10-27 21:44:49 +0530

    Re-Organization

---
+++ LearningDesignPatterns/Tests/Algorithms/LogicProgramsTests/Logics/MathTests.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LogicPrograms.Logics;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LogicPrograms.Logics.Tests
+{
+    [TestClass()]
+    public class MathTests
+    {
+        [TestMethod()]
+        public void GetFactorialTest()
+        {
+
+        }
+    }
+}
+
+namespace LogicProgramsTests.Logics
+{
+    class MathTests
+    {
+    }
+}
