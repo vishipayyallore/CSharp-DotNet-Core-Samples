cd8a6a8178d8671b1a1c7bf1bbbff28bb95b6520
Viswanatha Swamy <vishipayyallore@yahoo.com>
2019-10-30 23:00:24 +0530

    ..

--- LearningDesignPatterns/Source/Alogithms/LogicPrograms/Program.cs
+++ LearningDesignPatterns/Source/Alogithms/LogicPrograms/Program.cs
@@ -12,9 +12,8 @@ namespace LogicPrograms
             IMath math = new Math();
 
             Write("Enter a number for finding Factorial: ");
-            var value = ReadLine();
 
-            if (int.TryParse(value, out int number))
+            if (int.TryParse(ReadLine(), out int number))
             {
                 var factorial = math.GetFactorial(number);
                 WriteLine($"Factorial: {factorial}");
