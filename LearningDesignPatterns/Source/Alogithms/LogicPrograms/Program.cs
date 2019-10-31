7d2ab1f50cba901bf98b9ca500b89d9ce11290ba
Viswanatha Swamy <vishipayyallore@yahoo.com>
2019-10-31 22:20:01 +0530

    Sample Sorting Program

--- LearningDesignPatterns/Source/Alogithms/LogicPrograms/Program.cs
+++ LearningDesignPatterns/Source/Alogithms/LogicPrograms/Program.cs
@@ -9,6 +9,26 @@ namespace LogicPrograms
         static void Main(string[] args)
         {
 
+            //-------------------------------------------------------------
+            var arrayItems = new int[] { 1, 1, 0, 0, 1, 1, 0, 1, 0, 1, 0, 0 };
+            var oneCount = 0;
+
+            foreach(var item in arrayItems)
+            {
+                if(item == 1)
+                {
+                    oneCount++;
+                    continue;
+                }
+                Write($"{item} ");
+            }
+
+            for(var counter=0; counter < oneCount; counter++)
+            {
+                Write($"1 ");
+            }
+            //-------------------------------------------------------------
+
             IMath math = new Math();
 
             Write("Enter a number for finding Factorial: ");
