58127c457ce821f966860d914b4645e99aec92a0
Viswanatha Swamy <vishipayyallore@yahoo.com>
2019-11-02 22:02:42 +0530

    New Project

---
+++ LearningDesignPatterns/Source/Features/NewFeatures/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace NewFeatures
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+
+
+    public struct Person
+    {
+        public Guid Id { get; set; }
+
+        public string Name { get; set; }
+
+        public readonly override string ToString() => $"Id: {Id} | Name: {Name}";
+    }
+
+}
