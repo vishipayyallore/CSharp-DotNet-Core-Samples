202f86d8308994c364b74009fe974c56c796fbba
Viswanatha Swamy <vishipayyallore@yahoo.com>
2019-11-08 22:43:11 +0530

    Update Program.cs

--- LearningDesignPatterns/Source/DataStructures/LinkedLists/LinkedList.Demos/Program.cs
+++ LearningDesignPatterns/Source/DataStructures/LinkedLists/LinkedList.Demos/Program.cs
@@ -9,6 +9,17 @@ namespace LinkedList.Demos
     public class Node
     {
         public int Value { get; set; }
+
+        public Node NextNode { get; set; }
+
+        public override string ToString()
+        {
+            GCHandle handle = GCHandle.Alloc(this, GCHandleType.WeakTrackResurrection);
+            IntPtr address = GCHandle.ToIntPtr(handle);
+
+            return $"Value: {Value} at Address: {address}";
+        }
+
     }
 
     class Program
@@ -17,12 +28,13 @@ namespace LinkedList.Demos
         {
 
             var node = new Node { Value = 101 };
+            WriteLine(node);
 
-            GCHandle handle = GCHandle.Alloc(node, GCHandleType.WeakTrackResurrection);
+            //GCHandle handle = GCHandle.Alloc(node, GCHandleType.WeakTrackResurrection);
 
-            IntPtr address = GCHandle.ToIntPtr(handle);
+            //IntPtr address = GCHandle.ToIntPtr(handle);
 
-            WriteLine($"Value: {node.Value} at Address: {address}");
+            //WriteLine($"Value: {node.Value} at Address: {address}");
 
             WriteLine("\n\nPress any key ...");
             ReadKey();
