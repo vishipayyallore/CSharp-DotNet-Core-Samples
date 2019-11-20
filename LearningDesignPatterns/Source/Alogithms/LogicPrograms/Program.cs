212e10d9c87f4edaa660e9b60250e51ad606fba8
Viswanatha Swamy <vishipayyallore@yahoo.com>
2019-11-20 07:55:33 +0530

    Update Program.cs

--- LearningDesignPatterns/Source/Alogithms/LogicPrograms/Program.cs
+++ LearningDesignPatterns/Source/Alogithms/LogicPrograms/Program.cs
@@ -1,5 +1,4 @@
-﻿using LanguageExt;
-using LogicPrograms.Interfaces;
+﻿using LogicPrograms.Interfaces;
 using LogicPrograms.Logics;
 using System.Linq;
 using static System.Console;
@@ -13,7 +12,7 @@ namespace LogicPrograms
             int[] array1 = { 1, 3, 5, 7, 9 };
             int min = 0, max = 0;
 
-            for(int index=0; index < array1.Length; index++)
+            for (int index = 0; index < array1.Length; index++)
             {
                 var current = array1.Sum() - array1[index];
 
@@ -23,7 +22,7 @@ namespace LogicPrograms
                 }
                 else
                 {
-                    if(current > max)
+                    if (current > max)
                     {
                         max = current;
                     }
@@ -34,7 +33,7 @@ namespace LogicPrograms
                 }
                 WriteLine(array1.Sum() - array1[index]);
             }
-            WriteLine($"{min} {max}");            
+            WriteLine($"{min} {max}");
 
             GeneralPrograms generalPrograms = new GeneralPrograms();
             generalPrograms.DisplayStairCase(10);
@@ -94,3 +93,36 @@ namespace LogicPrograms
     }
 
 }
+
+
+//static Int64 GetSum(int[] array1)
+//{
+//    Int64 sum = 0;
+//    for (var index = 0; index < array1.Length; index++)
+//    {
+//        sum += array1[index];
+//    }
+
+//    return sum;
+//}
+
+//static void miniMaxSum(int[] array1)
+//{
+//    Int64 sum = GetSum(array1);
+//    Int64 min = sum, max = 0;
+
+//    for (int index = 0; index < array1.Length; index++)
+//    {
+//        Int64 current = sum - array1[index];
+
+//        if (current > max)
+//        {
+//            max = current;
+//        }
+//        if (current < min)
+//        {
+//            min = current;
+//        }
+//    }
+//    Console.WriteLine($"{min} {max}");
+//}
