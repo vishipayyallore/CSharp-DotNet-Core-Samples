d140094668446bcc904321b9908751d2fb260cd1
Viswanatha Swamy <vishipayyallore@yahoo.com>
2019-12-10 21:44:31 +0530

    Initial Version

---
+++ LearningCSharp/first-app/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace first_app
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
