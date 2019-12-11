04cc4d0d70f62191b965c889c0d8ae07ed1f0a8b
Viswanatha Swamy <vishipayyallore@yahoo.com>
2019-12-11 07:38:44 +0530

    Initial

---
+++ LearningCSharp/second-app/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace second_app
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
