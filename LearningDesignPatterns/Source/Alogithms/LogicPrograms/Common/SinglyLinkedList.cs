d4a6f3cf93731a2aaecc5395ce084e6b823d1448
Viswanatha Swamy <vishipayyallore@yahoo.com>
2020-01-11 11:36:48 +0530

    Create SinglyLinkedList.cs

---
+++ LearningDesignPatterns/Source/Alogithms/LogicPrograms/Common/SinglyLinkedList.cs
@@ -0,0 +1,31 @@
+﻿namespace LogicPrograms.Common
+{
+    public class SinglyLinkedList
+    {
+        public SinglyLinkedListNode Head;
+        public SinglyLinkedListNode Tail;
+
+        public SinglyLinkedList()
+        {
+            Head = null;
+            Tail = null;
+        }
+
+        public void InsertNode(int nodeData)
+        {
+            SinglyLinkedListNode node = new SinglyLinkedListNode(nodeData);
+
+            if (Head == null)
+            {
+                Head = node;
+            }
+            else
+            {
+                Tail.Next = node;
+            }
+
+            Tail = node;
+        }
+    }
+
+}
